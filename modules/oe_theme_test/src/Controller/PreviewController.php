93a4284213c8bbb869fb379a1a4ae58deb5b22d0
Antonio De Marco <antonio@nuvole.org>
2017-09-06 16:20:31 +0200

    Issue #9: Add basic preview page.

--- modules/oe_theme_test/src/Controller/PreviewController.php
+++ modules/oe_theme_test/src/Controller/PreviewController.php
@@ -3,8 +3,8 @@
 namespace Drupal\oe_theme_test\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\oe_theme_test\ThemePreviewManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Render\Renderer;
 
 /**
  * Class PreviewController.
@@ -12,17 +12,17 @@ use Drupal\Core\Render\Renderer;
 class PreviewController extends ControllerBase {
 
   /**
-   * Drupal\Core\Render\Renderer definition.
+   * Theme preview manager.
    *
-   * @var \Drupal\Core\Render\Renderer
+   * @var \Drupal\oe_theme_test\ThemePreviewManager
    */
-  protected $renderer;
+  protected $previewManager;
 
   /**
    * Constructs a new PreviewController object.
    */
-  public function __construct(Renderer $renderer) {
-    $this->renderer = $renderer;
+  public function __construct(ThemePreviewManager $preview_manager) {
+    $this->previewManager = $preview_manager;
   }
 
   /**
@@ -30,21 +30,27 @@ class PreviewController extends ControllerBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('renderer')
+      $container->get('plugin.manager.theme_preview')
     );
   }
 
   /**
    * Render.
    *
-   * @return string
+   * @return array
    *   Return Hello string.
    */
   public function render() {
-    return [
-      '#type' => 'markup',
-      '#markup' => $this->t('Implement method: render'),
-    ];
+    $render = [];
+    foreach ($this->previewManager->getDefinitions() as $id => $definition) {
+
+      $render[$id] = [
+        'label' => ['#markup' => $definition['label']],
+        'description' => ['#markup' => $definition['description']],
+        'preview' => $definition['preview'],
+      ];
+    }
+    return $render;
   }
 
 }
