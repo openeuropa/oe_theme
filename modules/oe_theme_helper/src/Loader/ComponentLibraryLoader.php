9d6091e8d2cba3a9bdd8c1d30ca81a7b3b427114
Pol Dellaiera <pol.dellaiera@protonmail.com>
2018-02-08 12:07:55 +0100

    OPENEUROPA-12: Include Europa Component Library Twig loader as composer dependency. (#19)

---
+++ modules/oe_theme_helper/src/Loader/ComponentLibraryLoader.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\oe_theme_helper\Loader;
+
+use Drupal\Core\Extension\ThemeHandler;
+use EC\Twig\Loader\EuropaComponentLibraryLoader;
+
+/**
+ * Class ComponentLibraryLoader.
+ */
+class ComponentLibraryLoader extends EuropaComponentLibraryLoader {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct($namespaces, $root, $theme, $directory, ThemeHandler $theme_handler) {
+    $theme_path = $theme_handler->getTheme($theme)->getPath();
+    parent::__construct($namespaces, $theme_path . DIRECTORY_SEPARATOR . $directory, $root);
+  }
+
+}
