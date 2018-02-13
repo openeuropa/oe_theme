2e2c4dd1a924bbb70e118847fbcfda5184968f16
Antonio De Marco <antonio@nuvole.org>
2018-02-13 07:45:50 +0100

    OPENEUROPA-193: Make demo blocks actual discoverable plugins.

---
+++ modules/oe_theme_demo/src/DemoBlockPluginManager.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\oe_theme_demo;
+
+use Drupal\Component\Plugin\Exception\PluginException;
+use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Plugin\DefaultPluginManager;
+use Drupal\Core\Plugin\Discovery\ContainerDerivativeDiscoveryDecorator;
+use Drupal\Core\Plugin\Discovery\YamlDiscovery;
+
+/**
+ * Provides the default demo_block manager.
+ */
+class DemoBlockPluginManager extends DefaultPluginManager implements DemoBlockPluginManagerInterface {
+
+  /**
+   * Provides default values for all demo_block plugins.
+   *
+   * @var array
+   */
+  protected $defaults = [
+    // Add required and optional plugin properties.
+    'id' => '',
+    'label' => '',
+  ];
+
+  /**
+   * Constructs a new DemoBlockPluginManager object.
+   *
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler.
+   * @param \Drupal\Core\Cache\CacheBackendInterface $cache_backend
+   *   Cache backend instance to use.
+   */
+  public function __construct(ModuleHandlerInterface $module_handler, CacheBackendInterface $cache_backend) {
+    // Add more services as required.
+    $this->moduleHandler = $module_handler;
+    $this->setCacheBackend($cache_backend, 'demo_block', ['demo_block']);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getDiscovery() {
+    if (!isset($this->discovery)) {
+      $this->discovery = new YamlDiscovery('demo_blocks', $this->moduleHandler->getModuleDirectories());
+      $this->discovery->addTranslatableProperty('label', 'label_context');
+      $this->discovery = new ContainerDerivativeDiscoveryDecorator($this->discovery);
+    }
+    return $this->discovery;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processDefinition(&$definition, $plugin_id) {
+    parent::processDefinition($definition, $plugin_id);
+
+    // You can add validation of the plugin definition here.
+    if (empty($definition['id'])) {
+      throw new PluginException(sprintf('Example plugin property (%s) definition "is" is required.', $plugin_id));
+    }
+  }
+
+  // Add other methods here as defined in the DemoBlockPluginManagerInterface.
+
+}
