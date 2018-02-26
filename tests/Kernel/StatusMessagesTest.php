a9a756378d3dd56663ca17c459d4606f9d6849e7
Francesco Sardara <francesco.sardara@one-agency.be>
2018-02-26 20:28:16 +0100

    OPENEUROPA-318: Improve test coverage for status messages.

--- tests/Kernel/StatusMessagesTest.php
+++ tests/Kernel/StatusMessagesTest.php
@@ -13,62 +13,193 @@ use Symfony\Component\DomCrawler\Crawler;
 class StatusMessagesTest extends AbstractKernelTest {
 
   /**
+   * The message types available.
+   *
+   * Each element contains:
+   *   - heading: the heading shown in the interface.
+   *   - modifier: the string appended as class modifier to the wrapper of the
+   *     message of a certain type.
+   *
+   * @var array
+   */
+  const MESSAGE_TYPES = [
+    MessengerInterface::TYPE_STATUS => [
+      'heading' => 'Status message',
+      'modifier' => 'success',
+    ],
+    MessengerInterface::TYPE_WARNING => [
+      'heading' => 'Warning message',
+      'modifier' => 'warning',
+    ],
+    MessengerInterface::TYPE_ERROR => [
+      'heading' => 'Error message',
+      'modifier' => 'error',
+    ],
+  ];
+
+  /**
    * Tests the rendering of the status messages.
    *
-   * @param string $type
-   *   The status message type being tested.
-   * @param string $heading
-   *   The heading text.
-   * @param string $modifier
-   *   The corresponding modifier for the status type in ECL.
+   * @param array $data
+   *   An array of messages, keyed by message type.
    *
-   * @dataProvider statusMessageTypesProvider
+   * @dataProvider statusMessagesProvider
    */
-  public function testStatusMessages(string $type, string $heading, string $modifier): void {
+  public function testStatusMessages(array $data): void {
     $messenger = \Drupal::messenger();
-    $messenger->addMessage("$type message 1.", $type);
-    $messenger->addMessage("$type message 2.", $type);
+
+    foreach ($data as $type => $messages) {
+      foreach ($messages as $message) {
+        $messenger->addMessage($message, $type);
+      }
+    }
+
     $render = ['#type' => 'status_messages'];
 
     $html = (string) \Drupal::service('renderer')->renderRoot($render);
     $crawler = new Crawler($html);
 
-    $wrapper = $crawler->filter('div.ecl-message--' . $modifier);
-    $this->assertCount(1, $wrapper);
+    foreach ($data as $type => $messages) {
+      $modifier = self::MESSAGE_TYPES[$type]['modifier'];
+      $heading = self::MESSAGE_TYPES[$type]['heading'];
 
-    $title = $wrapper->filter('div.ecl-message__title');
-    $this->assertCount(1, $title);
-    $this->assertEquals($heading, $title->first()->text());
+      $wrapper = $crawler->filter('div.ecl-message--' . $modifier);
+      $this->assertCount(1, $wrapper, sprintf('Wrong number of wrappers found for "%s" messages.', $type));
 
-    $list_items = $wrapper->filter('ul.ecl-message__body li');
-    $this->assertCount(2, $list_items);
+      $title = $wrapper->filter('div.ecl-message__title');
+      $this->assertCount(1, $title, sprintf('Wrong number of headings found for "%s" messages.', $type));
+      $this->assertEquals($heading, trim($title->first()->text()));
 
-    $this->assertEquals("$type message 1.", $list_items->eq(0)->text());
-    $this->assertEquals("$type message 2.", $list_items->eq(1)->text());
+      $list_items = $wrapper->filter('ul.ecl-message__body li');
+      $this->assertSameSize($messages, $list_items, sprintf('Wrong number of "%s" messages found.', $type));
+
+      foreach ($messages as $delta => $message) {
+        $this->assertEquals($message, trim($list_items->eq($delta)->text()));
+      }
+    }
+
+    // Verify that no message types other than the ones present in the test data
+    // are rendered.
+    foreach (array_diff_key(self::MESSAGE_TYPES, $data) as $type => $info) {
+      $wrapper = $crawler->filter('div.ecl-message--' . $info['modifier']);
+      $this->assertEmpty(
+        $wrapper,
+        sprintf('No messages of type "%s" were expected, but %d found.', $type, $wrapper->count())
+      );
+      $this->assertNotContains(
+        $info['heading'],
+        $html,
+        'No messages of type "%s" are present, but the related heading "%s" was found.', $type, $info['heading']
+      );
+    }
+
   }
 
   /**
-   * Data provider for status messages types.
+   * Data provider for status messages.
    *
    * @return array
    *   A list of test data.
    */
-  public function statusMessageTypesProvider(): array {
+  public function statusMessagesProvider(): array {
     return [
+      // A test case with no messages.
       [
-        MessengerInterface::TYPE_STATUS,
-        'Status message',
-        'success',
+        [],
       ],
+      // A test case with a single "status" message.
       [
-        MessengerInterface::TYPE_WARNING,
-        'Warning message',
-        'warning',
+        [
+          MessengerInterface::TYPE_STATUS => [
+            'Status message 1.',
+          ],
+        ],
       ],
+      // A test case with multiple "status" messages.
       [
-        MessengerInterface::TYPE_ERROR,
-        'Error message',
-        'error',
+        [
+          MessengerInterface::TYPE_STATUS => [
+            'Status message 1.',
+            'Status message 2.',
+          ],
+        ],
+      ],
+      // A test case with a single "warning" message.
+      [
+        [
+          MessengerInterface::TYPE_WARNING => [
+            'Warning message 1.',
+          ],
+        ],
+      ],
+      // A test case with multiple "warning" messages.
+      [
+        [
+          MessengerInterface::TYPE_WARNING => [
+            'Warning message 1.',
+            'Warning message 2.',
+            'Warning message 3.',
+          ],
+        ],
+      ],
+      // A test case with a single "error" message.
+      [
+        [
+          MessengerInterface::TYPE_ERROR => [
+            'Error message 1.',
+          ],
+        ],
+      ],
+      // A test case with multiple "error" messages.
+      [
+        [
+          MessengerInterface::TYPE_ERROR => [
+            'Error message 1.',
+            'Error message 2.',
+            'Error message 3.',
+            'Error message 4.',
+          ],
+        ],
+      ],
+      // A test case with two message types with one message each.
+      [
+        [
+          MessengerInterface::TYPE_STATUS => [
+            'Status message 1.',
+          ],
+          MessengerInterface::TYPE_WARNING => [
+            'Warning message 1.',
+          ],
+        ],
+      ],
+      // A test case with two message types with multiple messages for each one.
+      [
+        [
+          MessengerInterface::TYPE_ERROR => [
+            'Error message 1.',
+            'Error message 2.',
+          ],
+          MessengerInterface::TYPE_WARNING => [
+            'Warning message 1.',
+            'Warning message 2.',
+            'Warning message 3.',
+          ],
+        ],
+      ],
+      // A test case with all the message types with at least one message.
+      [
+        [
+          MessengerInterface::TYPE_STATUS => [
+            'Status message 1.',
+          ],
+          MessengerInterface::TYPE_WARNING => [
+            'Warning message 1.',
+          ],
+          MessengerInterface::TYPE_ERROR => [
+            'Error message 1.',
+            'Error message 2.',
+          ],
+        ],
       ],
     ];
   }
