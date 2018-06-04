6140435aee51efdbfcf968b546588a6f880277de
Antonio De Marco <antonio@nuvole.org>
2018-06-04 14:20:04 +0200

    OPENEUROPA-15: Add UI Patterns Library dependency.

---
+++ modules/oe_theme_demo/src/Routing/RouteSubscriber.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_theme_demo\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Alter UI Patterns Library overview page route.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterRoutes(RouteCollection $collection): void {
+    if ($route = $collection->get('ui_patterns.patterns.overview')) {
+      $route->setDefault('_title', 'Components');
+      $route->setPath('/components');
+    }
+  }
+
+}
