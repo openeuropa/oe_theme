c3930d3a2e4a188555857dd3c403e185d1da340c
Antonio De Marco <antonio@nuvole.org>
2018-07-17 17:17:33 +0200

    OPENEUROPA-712: Improve Behat test for language switcher block.

--- tests/Behat/MinkContext.php
+++ tests/Behat/MinkContext.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\oe_theme\Behat;
 
+use Behat\Mink\Element\NodeElement;
 use Drupal\DrupalExtension\Context\MinkContext as DrupalExtensionMinkContext;
 use Behat\Gherkin\Node\TableNode;
 use Drupal\Tests\oe_theme\Behat\Traits\UtilityTrait;
@@ -67,6 +68,36 @@ class MinkContext extends DrupalExtensionMinkContext {
     $this->getRegion('header')
       ->find('css', '.ecl-lang-select-sites__link')
       ->click();
+  }
+
+  /**
+   * Assert that given language switcher link is active.
+   *
+   * @throws \Behat\Mink\Exception\ElementNotFoundException
+   * @throws \Behat\Mink\Exception\ExpectationException
+   *
+   * @Then the active language switcher link in the dialog is :label
+   */
+  public function assertActiveLanguageSwitcherLink($label): void {
+    $selector = 'a.ecl-language-list__button--active';
+    $overlay = $this->getLanguageSwitcherOverlay();
+    $this->assertSession()->elementsCount('css', $selector, 1, $overlay);
+    $this->assertSession()->elementTextContains('css', $selector, $label);
+  }
+
+  /**
+   * Get language switcher overlay element.
+   *
+   * @return \Behat\Mink\Element\NodeElement
+   *   The language switcher overlay element.
+   *
+   * @throws \Behat\Mink\Exception\ElementNotFoundException
+   */
+  protected function getLanguageSwitcherOverlay(): NodeElement {
+    $selector = 'div.ecl-language-list.ecl-language-list--overlay';
+    $this->assertSession()->elementExists('css', $selector);
+
+    return $this->getSession()->getPage()->find('css', $selector);
   }
 
 }
