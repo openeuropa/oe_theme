71239a4f5cc051fb03625cc629acdbf0e343cba8
nagyad <joevagyok@gmail.com>
2018-11-06 14:11:28 +0100

    OPENEUROPA-1261: Implement file translations component.

---
+++ js/file_translations.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * ECL file_translations behavior.
+ */
+(function (ECL, Drupal) {
+    Drupal.behaviors.eclFileTranslations = {
+        attach: function attach() {
+            ECL.initExpandables('button.ecl-file__translations-toggle');
+            ECL.dropdown('div.ecl-file__translations-toggle');
+        }
+    };
+})(ECL, Drupal);
