5385ccd122764d988945a2b6ae49d69d8af2930b
nagyad <joevagyok@gmail.com>
2018-11-08 14:43:39 +0100

    OPENEUROPA-1261: Create documentation.

--- js/file_translations.js
+++ js/file_translations.js
@@ -3,10 +3,10 @@
  * ECL file_translations behavior.
  */
 (function (ECL, Drupal) {
-    Drupal.behaviors.eclFileTranslations = {
-        attach: function attach() {
-            ECL.initExpandables('button.ecl-file__translations-toggle');
-            ECL.dropdown('div.ecl-file__translations-toggle');
-        }
-    };
+  Drupal.behaviors.eclFileTranslations = {
+    attach: function attach() {
+      ECL.initExpandables('button.ecl-file__translations-toggle');
+      ECL.dropdown('div.ecl-file__translations-toggle');
+    }
+  };
 })(ECL, Drupal);
