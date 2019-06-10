f07e99a41970a0f5c96ed28251a73e5b86d7ed42
Antonio De Marco <antonio@nuvole.org>
2019-06-10 11:52:57 +0200

    OPENEUROPA-736: Prevent missing components from killing the site.

--- modules/oe_theme_helper/src/Loader/ComponentLibraryLoader.php
+++ modules/oe_theme_helper/src/Loader/ComponentLibraryLoader.php
@@ -5,25 +5,67 @@ declare(strict_types = 1);
 namespace Drupal\oe_theme_helper\Loader;
 
 use Drupal\Core\Extension\ThemeHandler;
+use Drupal\Core\Logger\LoggerChannelFactory;
 use OpenEuropa\Twig\Loader\EuropaComponentLibraryLoader;
+use Twig\Error\LoaderError;
+use Drupal\Core\Messenger\MessengerTrait;
 
 /**
- * Class ComponentLibraryLoader.
+ * Load ECL components Twig templates.
  */
 class ComponentLibraryLoader extends EuropaComponentLibraryLoader {
+
+  use MessengerTrait;
+
+  /**
+   * Theme path, if any.
+   *
+   * @var string
+   */
+  protected $themePath;
+
+  /**
+   * Logger factory.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelInterface
+   */
+  protected $logger;
 
   /**
    * {@inheritdoc}
    */
-  public function __construct($namespaces, $root, $theme, $directory, ThemeHandler $theme_handler) {
+  public function __construct($namespaces, $root, $theme, $directory, ThemeHandler $theme_handler, LoggerChannelFactory $logger_factory) {
     // Make sure the theme exists before getting its path.
     // This is necessary when the "oe_theme_helper" module is enabled before
     // the theme is or the theme is disabled and the "oe_theme_helper" is not.
     $path = '';
     if ($theme_handler->themeExists($theme)) {
-      $path = $theme_handler->getTheme($theme)->getPath() . DIRECTORY_SEPARATOR . $directory;
+      $this->themePath = $theme_handler->getTheme($theme)->getPath();
+      $path = $this->themePath . DIRECTORY_SEPARATOR . $directory;
     }
+
+    $this->logger = $logger_factory->get('ecl');
     parent::__construct($namespaces, $path, $root, '');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function findTemplate($name) {
+    // @todo: Remove this method override once the migration to ECL 2.0 is done.
+    try {
+      return parent::findTemplate($name);
+    }
+    catch (LoaderError $e) {
+      if ($this->themePath) {
+        $this->messenger()->addError("Missing component: {$name}", FALSE);
+        $this->logger->error("Missing component: @name", [
+          '@name' => $name,
+        ]);
+        return $this->themePath . '/templates/missing-component.html.twig';
+      }
+      return FALSE;
+    }
   }
 
 }
