fb2c2bb830828fdf69d0433235c91e0976b9341a
Voidtek <vdacosta@voidtek.com>
2019-06-21 17:56:44 +0100

    OPENEUROPA-1980: Upgrade Accordion to ECL2.

--- js/accordions.js
+++ js/accordions.js
@@ -5,7 +5,11 @@
 (function (ECL, Drupal) {
   Drupal.behaviors.eclAccordions = {
     attach: function attach() {
-      ECL.accordions();
+      var elements = document.querySelectorAll('[data-ecl-accordion]');
+      for (var i = 0; i < elements.length; i += 1) {
+        var accordion = new ECL.Accordion(elements[i]);
+        accordion.init();
+      }
     }
   };
 })(ECL, Drupal);
