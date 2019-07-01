176ea0a50cdb758c0ee77e419c7316b567de7465
Voidtek <vdacosta@voidtek.com>
2019-07-01 09:01:29 +0100

    OPENEUROPA-1984: Add the contextual navigation component.

--- js/context_nav.js
+++ js/context_nav.js
@@ -5,7 +5,11 @@
 (function (ECL, Drupal) {
   Drupal.behaviors.eclContextualNavs = {
     attach: function attach() {
-      ECL.contextualNavs();
+      var elements = document.querySelectorAll('[data-ecl-contextual-navigation]');
+      for (var i = 0; i < elements.length; i += 1) {
+        var contextualNavigation = new ECL.ContextualNavigation(elements[i]);
+        contextualNavigation.init();
+      }
     }
   };
 })(ECL, Drupal);
