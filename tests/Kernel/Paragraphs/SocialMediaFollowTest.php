b27fe7578ab73e9357dbd1d490582cf0ed92e21f
Alexandra <alexandra@Alexandras-MacBook-Pro.local>
2019-10-23 08:43:58 +0200

    OPENEUROPA-2342: Update the test.

--- tests/Kernel/Paragraphs/SocialMediaFollowTest.php
+++ tests/Kernel/Paragraphs/SocialMediaFollowTest.php
@@ -35,24 +35,29 @@ class SocialMediaFollowTest extends ParagraphsTestBase {
     // Create social media link items.
     $items = [
       [
-        'title' => 'Facebook',
+        'title' => 'Email',
+        'uri' => 'mailto:example@com',
+        'link_type' => 'email',
+      ],
+      [
+        'title' => 'Facebookk',
         'uri' => 'https://facebook.com',
-        'link_type' => 'Facebook',
+        'link_type' => 'facebook',
       ],
       [
         'title' => 'Flickr',
         'uri' => 'https://www.flickr.com',
-        'link_type' => 'Flickr',
+        'link_type' => 'flickr',
       ],
       [
         'title' => 'Google+',
         'uri' => 'https://google.com',
-        'link_type' => 'Google+',
+        'link_type' => 'google',
       ],
       [
         'title' => 'Instagram',
         'uri' => 'https://instagram.com',
-        'link_type' => 'Instagram',
+        'link_type' => 'instagram',
       ],
       [
         'title' => 'LinkedIn',
@@ -62,32 +67,32 @@ class SocialMediaFollowTest extends ParagraphsTestBase {
       [
         'title' => 'Pinterest',
         'uri' => 'https://pinterest.com',
-        'link_type' => 'Pinterest',
+        'link_type' => 'pinterest',
       ],
       [
         'title' => 'RSS',
         'uri' => 'https://rss-example.com',
-        'link_type' => 'RSS',
+        'link_type' => 'rss',
       ],
       [
         'title' => 'Storify',
         'uri' => 'https://storify.com',
-        'link_type' => 'Storify',
+        'link_type' => 'storify',
       ],
       [
         'title' => 'Twitter',
         'uri' => 'https://twitter.com',
-        'link_type' => 'Twitter',
+        'link_type' => 'twitter',
       ],
       [
         'title' => 'Yammer',
         'uri' => 'https://yammer.com',
-        'link_type' => 'Yammer',
+        'link_type' => 'yammer',
       ],
       [
         'title' => 'Youtube',
         'uri' => 'https://youtube.com',
-        'link_type' => 'YouTube',
+        'link_type' => 'youtube',
       ],
     ];
 
@@ -111,8 +116,9 @@ class SocialMediaFollowTest extends ParagraphsTestBase {
     $html = $this->renderParagraph($paragraph);
     $crawler = new Crawler($html);
 
-    // Verify the vertical layout.
+    // Verify that the horizontal layout is not present.
     $this->assertCount(0, $crawler->filter('.ecl-social-media-link--horizontal'));
+    // Verify the default ecl layout (vertical layout).
     $this->assertCount(1, $crawler->filter('.ecl-social-media-link'));
 
     // Verify that links are rendered.
@@ -121,17 +127,30 @@ class SocialMediaFollowTest extends ParagraphsTestBase {
 
     // Verify that the paragraph contains all the links.
     $links_html = $links->html();
+    $this->assertContains('Email', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--email.ecl-social-media-link__link'));
     $this->assertContains('Facebook', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--facebook.ecl-social-media-link__link'));
     $this->assertContains('Flickr', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--flickr.ecl-social-media-link__link'));
     $this->assertContains('Google+', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--google.ecl-social-media-link__link'));
     $this->assertContains('Instagram', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--instagram.ecl-social-media-link__link'));
     $this->assertContains('LinkedIn', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--linkedin.ecl-social-media-link__link'));
     $this->assertContains('Pinterest', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--pinterest.ecl-social-media-link__link'));
     $this->assertContains('RSS', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--rss.ecl-social-media-link__link'));
     $this->assertContains('Storify', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--storify.ecl-social-media-link__link'));
     $this->assertContains('Twitter', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--twitter.ecl-social-media-link__link'));
     $this->assertContains('Yammer', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--yammer.ecl-social-media-link__link'));
     $this->assertContains('Youtube', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--youtube.ecl-social-media-link__link'));
   }
 
 }
