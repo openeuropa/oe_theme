3096f8ca998e3a72b7bc6c0d447aaca074c2c51f
Francesco Sardara <francesco.sardara@ausy.be>
2019-11-04 14:48:49 +0100

    OPENEUROPA-2342: Minor code refactors.

--- tests/Kernel/Paragraphs/SocialMediaFollowTest.php
+++ tests/Kernel/Paragraphs/SocialMediaFollowTest.php
@@ -13,19 +13,94 @@ use Symfony\Component\DomCrawler\Crawler;
 class SocialMediaFollowTest extends ParagraphsTestBase {
 
   /**
-   * Creates link items for Social media follow paragraph.
+   * Tests the rendering of the paragraph type.
+   *
+   * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+   */
+  public function testRendering(): void {
+    // Create social media follow paragraph with horizontal variant.
+    $paragraph = Paragraph::create([
+      'type' => 'oe_social_media_follow',
+      'field_oe_title' => 'Social media title',
+      'field_oe_social_media_variant' => 'horizontal',
+      'field_oe_social_media_links' => $this->getSocialMediaLinks(),
+    ]);
+    $paragraph->save();
+    $html = $this->renderParagraph($paragraph);
+    $crawler = new Crawler($html);
+
+    // Verify that there is a horizontal layout.
+    $this->assertCount(1, $crawler->filter('.ecl-social-media-link.ecl-social-media-link--horizontal'));
+
+    // Change variant to vertical layout.
+    $paragraph->get('field_oe_social_media_variant')->setValue('vertical');
+    $paragraph->save();
+    $html = $this->renderParagraph($paragraph);
+    $crawler = new Crawler($html);
+
+    // Verify that the horizontal layout is not present.
+    $this->assertCount(0, $crawler->filter('.ecl-social-media-link--horizontal'));
+    // Verify the default ecl layout (vertical layout).
+    $this->assertCount(1, $crawler->filter('.ecl-social-media-link'));
+
+    // Verify that links are rendered.
+    $links = $crawler->filter('.ecl-social-media-link__list.ecl-list.ecl-list--unstyled');
+    $this->assertCount(1, $links);
+
+    // Verify that the paragraph contains all the links.
+    $links_html = $links->html();
+    $this->assertContains('Email', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--email.ecl-social-media-link__link'));
+    // Assert that the Facebook link is rendered but not the Facebook icon, as
+    // this link has no type associated.
+    $this->assertContains('Facebook', $links_html);
+    $this->assertCount(0, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--facebook.ecl-social-media-link__link'));
+    $this->assertContains('Flickr', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--flickr.ecl-social-media-link__link'));
+    $this->assertContains('Google+', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--google.ecl-social-media-link__link'));
+    $this->assertContains('Instagram', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--instagram.ecl-social-media-link__link'));
+    $this->assertContains('LinkedIn', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--linkedin.ecl-social-media-link__link'));
+    $this->assertContains('Pinterest', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--pinterest.ecl-social-media-link__link'));
+    $this->assertContains('RSS', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--rss.ecl-social-media-link__link'));
+    $this->assertContains('Storify', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--storify.ecl-social-media-link__link'));
+    $this->assertContains('1st Twitter', $links_html);
+    $this->assertContains('2nd Twitter', $links_html);
+    $this->assertCount(2, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--twitter.ecl-social-media-link__link'));
+    $this->assertContains('Yammer', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--yammer.ecl-social-media-link__link'));
+    $this->assertContains('Youtube', $links_html);
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--youtube.ecl-social-media-link__link'));
+
+    // Fix the Facebook link type.
+    $paragraph->get('field_oe_social_media_links')->get(1)->set('link_type', 'facebook');
+    $paragraph->save();
+    $html = $this->renderParagraph($paragraph);
+    $crawler = new Crawler($html);
+
+    // Verify that the Facebook icon is now rendered.
+    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--facebook.ecl-social-media-link__link'));
+  }
+
+  /**
+   * Returns a list of link items for Social media follow paragraph.
    *
    * @return array
-   *   Returns array of link items.
+   *   An array of link items.
    */
   protected function getSocialMediaLinks(): array {
-    // Create social media link items.
-    $items = [
+    return [
       [
         'title' => 'Email',
         'uri' => 'mailto:example@com',
         'link_type' => 'email',
       ],
+      // Add a Facebook link without type.
       [
         'title' => 'Facebook',
         'uri' => 'https://facebook.com',
@@ -92,86 +167,6 @@ class SocialMediaFollowTest extends ParagraphsTestBase {
         'link_type' => 'vimeo',
       ],
     ];
-
-    return $items;
-  }
-
-  /**
-   * Tests the rendering of the paragraph type.
-   *
-   * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
-   */
-  public function testRendering(): void {
-    // Create social media follow paragraph with horizontal variant.
-    $paragraph = Paragraph::create([
-      'type' => 'oe_social_media_follow',
-      'field_oe_title' => 'Social media title',
-      'field_oe_social_media_variant' => 'horizontal',
-      'field_oe_social_media_links' => $this->getSocialMediaLinks(),
-    ]);
-    $paragraph->save();
-    $html = $this->renderParagraph($paragraph);
-    $crawler = new Crawler($html);
-
-    // Verify that there is a horizontal layout.
-    $this->assertCount(1, $crawler->filter('.ecl-social-media-link.ecl-social-media-link--horizontal'));
-
-    // Change variant to vertical layout.
-    $paragraph->get('field_oe_social_media_variant')->setValue('vertical');
-    $paragraph->save();
-    $html = $this->renderParagraph($paragraph);
-    $crawler = new Crawler($html);
-
-    // Verify that the horizontal layout is not present.
-    $this->assertCount(0, $crawler->filter('.ecl-social-media-link--horizontal'));
-    // Verify the default ecl layout (vertical layout).
-    $this->assertCount(1, $crawler->filter('.ecl-social-media-link'));
-
-    // Verify that links are rendered.
-    $links = $crawler->filter('.ecl-social-media-link__list.ecl-list.ecl-list--unstyled');
-    $this->assertCount(1, $links);
-
-    // Verify that the paragraph contains all the links.
-    $links_html = $links->html();
-    $this->assertContains('Email', $links_html);
-    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--email.ecl-social-media-link__link'));
-    $this->assertContains('Facebook', $links_html);
-    $this->assertCount(0, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--facebook.ecl-social-media-link__link'));
-    $this->assertContains('Flickr', $links_html);
-    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--flickr.ecl-social-media-link__link'));
-    $this->assertContains('Google+', $links_html);
-    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--google.ecl-social-media-link__link'));
-    $this->assertContains('Instagram', $links_html);
-    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--instagram.ecl-social-media-link__link'));
-    $this->assertContains('LinkedIn', $links_html);
-    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--linkedin.ecl-social-media-link__link'));
-    $this->assertContains('Pinterest', $links_html);
-    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--pinterest.ecl-social-media-link__link'));
-    $this->assertContains('RSS', $links_html);
-    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--rss.ecl-social-media-link__link'));
-    $this->assertContains('Storify', $links_html);
-    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--storify.ecl-social-media-link__link'));
-    $this->assertContains('1st Twitter', $links_html);
-    $this->assertContains('2nd Twitter', $links_html);
-    $this->assertCount(2, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--twitter.ecl-social-media-link__link'));
-    $this->assertContains('Yammer', $links_html);
-    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--yammer.ecl-social-media-link__link'));
-    $this->assertContains('Youtube', $links_html);
-    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--youtube.ecl-social-media-link__link'));
-
-    // Change variant to vertical layout.
-    $links = $paragraph->get('field_oe_social_media_links')->getValue();
-    foreach ($links as $key => $link) {
-      if ($link['title'] == 'Facebook') {
-        $links[$key]['link_type'] = 'facebook';
-      }
-    }
-    $paragraph->get('field_oe_social_media_links')->setValue($links);
-    $paragraph->save();
-    $html = $this->renderParagraph($paragraph);
-    $crawler = new Crawler($html);
-
-    $this->assertCount(1, $crawler->filter('.ecl-link.ecl-social-icon.ecl-social-icon--facebook.ecl-social-media-link__link'));
   }
 
 }
