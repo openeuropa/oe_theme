1c20797600f5061f0c13ca8aca95dfa8f45d5769
Antonio De Marco <antonio@nuvole.org>
2019-11-19 11:03:26 +0100

    OPENEUROPA-2482: Ensure that link patterns are correctly rendered when passing an URL object.

---
+++ tests/Kernel/Patterns/LinkPatternRenderingTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_theme\Kernel\Patterns;
+
+use Drupal\Core\Url;
+use Drupal\Tests\oe_theme\Kernel\AbstractKernelTestBase;
+
+/**
+ * Test link pattern rendering.
+ */
+class LinkPatternRenderingTest extends AbstractKernelTestBase {
+
+  /**
+   * Test that link patterns are correctly rendered when passing an URL object.
+   *
+   * @throws \Exception
+   */
+  public function testLinkPatternRendering() {
+    $pattern = [
+      '#type' => 'pattern',
+      '#id' => 'link',
+      '#fields' => [
+        'text' => 'Link text',
+        'url' => Url::fromUserInput('/', [
+          'attributes' => [
+            'class' => ['foo'],
+          ],
+        ]),
+      ],
+    ];
+
+    $html = $this->renderRoot($pattern);
+    $this->assertEquals('<a href="/" class="foo ecl-link ecl-link--standalone">Link text</a>', trim($html));
+  }
+
+}
