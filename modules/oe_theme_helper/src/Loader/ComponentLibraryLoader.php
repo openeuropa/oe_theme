c2dd6413487894ad48f1ac69cbe7d8711329824d
Antonio De Marco <antonio@nuvole.org>
2019-11-27 14:18:28 +0100

    OPENEUROPA-0000: Remove missing component template.

--- modules/oe_theme_helper/src/Loader/ComponentLibraryLoader.php
+++ modules/oe_theme_helper/src/Loader/ComponentLibraryLoader.php
@@ -7,7 +7,6 @@ namespace Drupal\oe_theme_helper\Loader;
 use Drupal\Core\Extension\ThemeHandler;
 use Drupal\Core\Logger\LoggerChannelFactory;
 use OpenEuropa\Twig\Loader\EuropaComponentLibraryLoader;
-use Twig\Error\LoaderError;
 use Drupal\Core\Messenger\MessengerTrait;
 
 /**
@@ -46,24 +45,6 @@ class ComponentLibraryLoader extends EuropaComponentLibraryLoader {
 
     $this->logger = $logger_factory->get('ecl');
     parent::__construct($namespaces, $path, $root);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function findTemplate($name) {
-    // @todo: Remove this method override once first stable release is out.
-    try {
-      return parent::findTemplate($name);
-    }
-    catch (LoaderError $e) {
-      if ($this->themePath) {
-        $this->messenger()->addError($e->getMessage(), FALSE);
-        $this->logger->error($e->getMessage());
-        return $this->themePath . '/templates/missing-component.html.twig';
-      }
-      return FALSE;
-    }
   }
 
 }
