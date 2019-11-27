e2c5865d39e497f7982771c0103315676ca9bdc2
Antonio De Marco <antonio@nuvole.org>
2019-11-27 14:23:23 +0100

    OPENEUROPA-0000: Remove deprecated properties.

--- modules/oe_theme_helper/src/Plugin/PageHeaderMetadata/NodeViewRoutesBase.php
+++ modules/oe_theme_helper/src/Plugin/PageHeaderMetadata/NodeViewRoutesBase.php
@@ -5,10 +5,7 @@ declare(strict_types = 1);
 namespace Drupal\oe_theme_helper\Plugin\PageHeaderMetadata;
 
 use Drupal\Core\Cache\CacheableMetadata;
-use Drupal\Core\Entity\EntityRepositoryInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\node\NodeInterface;
 use Drupal\oe_theme_helper\Event\NodeMetadataEvent;
 use Drupal\oe_theme_helper\PageHeaderMetadataEvents;
@@ -26,30 +23,6 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 abstract class NodeViewRoutesBase extends PageHeaderMetadataPluginBase implements ContainerFactoryPluginInterface {
 
   /**
-   * The current route match.
-   *
-   * @var \Drupal\Core\Routing\RouteMatchInterface
-   * @deprecated Will be removed in 2.x version.
-   */
-  protected $currentRouteMatch;
-
-  /**
-   * The entity repository.
-   *
-   * @var \Drupal\Core\Entity\EntityRepositoryInterface
-   * @deprecated Will be removed in 2.x version.
-   */
-  protected $entityRepository;
-
-  /**
-   * The entity type manager.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   * @deprecated Will be removed in 2.x version.
-   */
-  protected $entityTypeManager;
-
-  /**
    * The event dispatcher.
    *
    * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
@@ -65,21 +38,12 @@ abstract class NodeViewRoutesBase extends PageHeaderMetadataPluginBase implement
    *   The plugin_id for the plugin instance.
    * @param mixed $plugin_definition
    *   The plugin implementation definition.
-   * @param \Drupal\Core\Routing\RouteMatchInterface $current_route_match
-   *   The current route match.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
-   *   The entity repository.
    * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $event_dispatcher
    *   The event dispatcher.
    */
-  public function __construct(array $configuration, string $plugin_id, $plugin_definition, RouteMatchInterface $current_route_match, EntityTypeManagerInterface $entity_type_manager, EntityRepositoryInterface $entity_repository, EventDispatcherInterface $event_dispatcher) {
+  public function __construct(array $configuration, string $plugin_id, $plugin_definition, EventDispatcherInterface $event_dispatcher) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
 
-    $this->currentRouteMatch = $current_route_match;
-    $this->entityTypeManager = $entity_type_manager;
-    $this->entityRepository = $entity_repository;
     $this->eventDispatcher = $event_dispatcher;
   }
 
@@ -91,9 +55,6 @@ abstract class NodeViewRoutesBase extends PageHeaderMetadataPluginBase implement
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('current_route_match'),
-      $container->get('entity_type.manager'),
-      $container->get('entity.repository'),
       $container->get('event_dispatcher')
     );
   }
