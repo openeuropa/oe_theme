bb0d1dfe1033c10cd2ef6b8f736478777e5d9b78
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2019-11-27 15:47:02 +0100

    OPENEUROPA-2357: Theming and tests.

---
+++ tests/Kernel/Webtools/SocialShareTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_theme\Kernel\Webtools;
+
+use Drupal\Tests\oe_theme\Kernel\AbstractKernelTestBase;
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * Test share this block rendering.
+ */
+class SocialShareTest extends AbstractKernelTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'oe_webtools_social_share',
+  ];
+
+  /**
+   * Test Social share block rendering.
+   */
+  public function testSocialShareBlockRendering(): void {
+    // Setup and render social share block.
+    $config = [
+      'id' => 'social_share',
+      'label' => 'OpenEuropa Social Share block',
+      'provider' => 'oe_webtools_social_share',
+      'label_display' => '0',
+    ];
+
+    $render = $this->buildBlock('social_share', $config);
+
+    $html = (string) $this->container->get('renderer')->renderRoot($render);
+    $crawler = new Crawler($html);
+  }
+
+}
