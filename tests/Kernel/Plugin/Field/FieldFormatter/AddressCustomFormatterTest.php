49307fc4a496f6080716932b2d6b4a3a31933fe9
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2019-12-17 12:28:30 +0100

    OPENEUROPA-2642: Add Kernel tests for formatter.

---
+++ tests/Kernel/Plugin/Field/FieldFormatter/AddressCustomFormatterTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_theme_helper\Plugin\Field\FieldFormatter;
+
+use Drupal\entity_test\Entity\EntityTest;
+use Drupal\Tests\address\Kernel\Formatter\FormatterTestBase;
+
+/**
+ * Test AddressCustomFormatter plugin.
+ */
+class AddressCustomFormatterTest extends FormatterTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'oe_theme_helper',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    $this->createField('address', 'address_custom');
+  }
+
+  /**
+   * Tests belgian address formatting.
+   */
+  public function testCustomFormatterAddress() {
+    $entity = EntityTest::create([]);
+    $entity->{$this->fieldName} = [
+      'country_code' => 'BE',
+      'locality' => 'Brussels',
+      'postal_code' => '1000',
+      'address_line1' => 'Rue de la Loi, 56',
+    ];
+
+    $this->renderEntityFields($entity, $this->display);
+    $expected = 'Rue de la Loi, 56, 1000, Brussels';
+    $this->assertRaw($expected);
+  }
+
+}
