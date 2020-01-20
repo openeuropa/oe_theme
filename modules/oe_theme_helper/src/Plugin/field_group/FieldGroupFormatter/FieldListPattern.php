2f26e432abbb7cfa9382317ca380a873db7f4531
Antonio De Marco <antonio@nuvole.org>
2020-01-20 14:16:59 +0100

    OPENEUROPA-2221: Add initial formatter class.

---
+++ modules/oe_theme_helper/src/Plugin/field_group/FieldGroupFormatter/FieldListPattern.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_theme_helper\Plugin\field_group\FieldGroupFormatter;
+
+use Drupal\Core\Render\Element;
+
+/**
+ * Format a field group using the field list pattern.
+ *
+ * @FieldGroupFormatter(
+ *   id = "oe_theme_helper_field_list_pattern",
+ *   label = @Translation("Field list pattern"),
+ *   description = @Translation("Format a field group using the field list pattern."),
+ *   supported_contexts = {
+ *     "view"
+ *   }
+ * )
+ */
+class FieldListPattern extends PatternFormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getPatternId(): string {
+    return 'field_list';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preRender(&$element, $rendering_object) {
+    // Instantiate pattern render array.
+    $pattern = [
+      '#type' => 'pattern',
+      '#id' => $this->getPatternId(),
+      '#variant' => $this->getSetting('variant'),
+      '#fields' => [],
+    ];
+
+    foreach (Element::children($element) as $field_name) {
+      // Assign field label and content to the pattern's fields.
+      $pattern['#fields']['items'][] = [
+        'label' => $element[$field_name]['#title'],
+        'body' => [
+          '#label_display' => 'hidden',
+          '#theme' => 'field',
+        ] + $element[$field_name],
+      ];
+
+      // Remove field render array from the field group element.
+      unset($element[$field_name]);
+    }
+
+    // Pass along the pattern render array.
+    $element['pattern'] = $pattern;
+  }
+
+}
---
+++ modules/oe_theme_helper/src/Plugin/field_group/FieldGroupFormatter/PatternFormatterBase.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_theme_helper\Plugin\field_group\FieldGroupFormatter;
+
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\field_group\FieldGroupFormatterBase;
+use Drupal\ui_patterns\UiPatternsManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Base class for field group formatters that use a pattern for rendering.
+ */
+abstract class PatternFormatterBase extends FieldGroupFormatterBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * UI Patterns manager.
+   *
+   * @var \Drupal\ui_patterns\UiPatternsManager
+   */
+  protected $patternsManager;
+
+  /**
+   * PatternFormatterBase constructor.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\ui_patterns\UiPatternsManager $patterns_manager
+   *   UI Patterns manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, UiPatternsManager $patterns_manager) {
+    parent::__construct($plugin_id, $plugin_definition, $configuration['group'], $configuration['settings'], $configuration['label']);
+    $this->configuration = $configuration;
+    $this->patternsManager = $patterns_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('plugin.manager.ui_patterns')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'label' => '',
+      'variant' => '',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm() {
+    $pattern = $this->patternsManager->getDefinition($this->getPatternId());
+
+    $form['label'] = [
+      '#type' => 'textfield',
+      '#title' => t('Field group label'),
+      '#default_value' => $this->label,
+    ];
+
+    $form['variant'] = [
+      '#title' => $this->t('Variant'),
+      '#type' => 'select',
+      '#options' => $pattern->getVariantsAsOptions(),
+      '#default_value' => $this->getSetting('variant'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+
+    if ($this->getSetting('label')) {
+      $summary[] = $this->t('Label: @label', ['@label' => $this->getSetting('label')]);
+    }
+
+    if ($this->getSetting('variant')) {
+      $summary[] = $this->t('Variant: @variant', ['@variant' => $this->getSetting('variant')]);
+    }
+
+    return $summary;
+  }
+
+  /**
+   * Return pattern ID for the current formatter plugin.
+   *
+   * @return string
+   *   Pattern ID.
+   */
+  abstract protected function getPatternId(): string;
+
+}
