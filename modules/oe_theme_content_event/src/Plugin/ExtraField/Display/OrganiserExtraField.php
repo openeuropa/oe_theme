f39beb54fa3a3afeaa324b91576cf43e352a36e2
Antonio De Marco <antonio@nuvole.org>
2020-01-21 12:14:06 +0100

    OPENEUROPA-2221: Add organiser extra field.

---
+++ modules/oe_theme_content_event/src/Plugin/ExtraField/Display/OrganiserExtraField.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_theme_content_event\Plugin\ExtraField\Display;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\extra_field\Plugin\ExtraFieldDisplayBase;
+
+/**
+ * Extra field displaying organiser information on events.
+ *
+ * @ExtraFieldDisplay(
+ *   id = "oe_theme_content_event_organiser",
+ *   label = @Translation("Organiser"),
+ *   bundles = {
+ *     "node.oe_event",
+ *   },
+ *   visible = true
+ * )
+ */
+class OrganiserExtraField extends ExtraFieldDisplayBase {
+
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLabel() {
+    return $this->t('Organiser');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function view(ContentEntityInterface $entity) {
+    $is_internal = $entity->get('oe_event_organiser_is_internal')->value === 1;
+
+    // If the organiser is internal and not empty, show it.
+    if ($is_internal && !$entity->get('oe_event_organiser_internal')->isEmpty()) {
+      return [
+        '#markup' => $entity->get('oe_event_organiser_internal')->entity->label(),
+      ];
+    }
+
+    // If the organiser is not internal and not empty, show it.
+    if (!$is_internal && !$entity->get('oe_event_organiser_name')->isEmpty()) {
+      return [
+        '#markup' => $entity->get('oe_event_organiser_name')->value,
+      ];
+    }
+
+    // Return an empty array otherwise, so the field can be considered empty.
+    return [];
+  }
+
+}
