945ccf24b8ff2e2141976985d003e7673fa9320c
Antonio De Marco <antonio@nuvole.org>
2020-01-26 17:16:52 +0100

    OPENEUROPA-2221: Improve social media formatter.

--- modules/oe_theme_helper/src/Plugin/Field/FieldFormatter/SocialMediaLinksFormatter.php
+++ modules/oe_theme_helper/src/Plugin/Field/FieldFormatter/SocialMediaLinksFormatter.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace Drupal\oe_theme_helper\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\link\Plugin\Field\FieldFormatter\LinkFormatter;
 
 /**
@@ -29,6 +30,74 @@ class SocialMediaLinksFormatter extends LinkFormatter {
   /**
    * {@inheritdoc}
    */
+  public static function defaultSettings() {
+    return [
+      'title' => 'Social media',
+      'variant' => 'horizontal',
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $settings = $this->getSettings();
+    $summary = [];
+
+    if (!empty($settings['trim_length'])) {
+      $summary[] = t('Link text trimmed to @limit characters', ['@limit' => $settings['trim_length']]);
+    }
+    else {
+      $summary[] = t('Link text not trimmed');
+    }
+
+    if (!empty($settings['title'])) {
+      $summary[] = t('Block title: @title', ['@title' => $settings['title']]);
+    }
+
+    if (!empty($settings['variant'])) {
+      $summary[] = t('Pattern variant: @variant', ['@variant' => $settings['variant']]);
+    }
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $elements = parent::settingsForm($form, $form_state);
+
+    $elements['title'] = [
+      '#type' => 'textfield',
+      '#title' => t('Block title'),
+      '#default_value' => $this->getSetting('title'),
+      '#required' => TRUE,
+    ];
+
+    $elements['variant'] = [
+      '#title' => $this->t('Variant'),
+      '#type' => 'select',
+      '#options' => [
+        'horizontal' => $this->t('Horizontal'),
+        'vertical' => $this->t('Vertical'),
+      ],
+      '#default_value' => $this->getSetting('variant'),
+      '#required' => TRUE,
+    ];
+
+    // Hide following settings as their behaviour is controlled by the pattern.
+    $elements['url_only']['#access'] = FALSE;
+    $elements['url_plain']['#access'] = FALSE;
+    $elements['rel']['#access'] = FALSE;
+    $elements['target']['#access'] = FALSE;
+
+    return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     if (!$items->count()) {
       return [];
@@ -36,9 +105,9 @@ class SocialMediaLinksFormatter extends LinkFormatter {
 
     $pattern = [
       '#type' => 'pattern',
-      '#id' => 'social_media_links_horizontal',
+      '#id' => 'social_media_links_' . $this->getSetting('variant'),
       '#fields' => [
-        'title' => $this->t('Social media'),
+        'title' => $this->getSetting('title'),
         'links' => [],
       ],
     ];
