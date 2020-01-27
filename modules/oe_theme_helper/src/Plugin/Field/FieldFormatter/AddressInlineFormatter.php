023d36b5d469cf27ed4fefc6734b131a483b5f68
Antonio De Marco <antonio@nuvole.org>
2020-01-27 13:47:01 +0100

    OPENEUROPA-2642: Use AddressDefaultFormatter as base class again.

--- modules/oe_theme_helper/src/Plugin/Field/FieldFormatter/AddressInlineFormatter.php
+++ modules/oe_theme_helper/src/Plugin/Field/FieldFormatter/AddressInlineFormatter.php
@@ -4,21 +4,13 @@ declare(strict_types = 1);
 
 namespace Drupal\oe_theme_helper\Plugin\Field\FieldFormatter;
 
-use CommerceGuys\Addressing\AddressFormat\AddressField;
-use CommerceGuys\Addressing\AddressFormat\AddressFormat;
-use CommerceGuys\Addressing\AddressFormat\AddressFormatRepositoryInterface;
-use CommerceGuys\Addressing\Country\CountryRepositoryInterface;
 use CommerceGuys\Addressing\Locale;
-use CommerceGuys\Addressing\Subdivision\SubdivisionRepositoryInterface;
 use Drupal\address\AddressInterface;
+use Drupal\address\Plugin\Field\FieldFormatter\AddressDefaultFormatter;
 use Drupal\Component\Utility\Html;
-use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
-use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Language\LanguageInterface;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Format an address inline with locale format and a configurable separator.
@@ -31,81 +23,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   },
  * )
  */
-class AddressInlineFormatter extends FormatterBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * The address format repository.
-   *
-   * @var \CommerceGuys\Addressing\AddressFormat\AddressFormatRepositoryInterface
-   */
-  protected $addressFormatRepository;
-
-  /**
-   * The country repository.
-   *
-   * @var \CommerceGuys\Addressing\Country\CountryRepositoryInterface
-   */
-  protected $countryRepository;
-
-  /**
-   * The subdivision repository.
-   *
-   * @var \CommerceGuys\Addressing\Subdivision\SubdivisionRepositoryInterface
-   */
-  protected $subdivisionRepository;
-
-  /**
-   * Constructs an AddressDefaultFormatter object.
-   *
-   * @param string $plugin_id
-   *   The plugin_id for the formatter.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
-   *   The definition of the field to which the formatter is associated.
-   * @param array $settings
-   *   The formatter settings.
-   * @param string $label
-   *   The formatter label display setting.
-   * @param string $view_mode
-   *   The view mode.
-   * @param array $third_party_settings
-   *   Any third party settings.
-   * @param \CommerceGuys\Addressing\AddressFormat\AddressFormatRepositoryInterface $address_format_repository
-   *   The address format repository.
-   * @param \CommerceGuys\Addressing\Country\CountryRepositoryInterface $country_repository
-   *   The country repository.
-   * @param \CommerceGuys\Addressing\Subdivision\SubdivisionRepositoryInterface $subdivision_repository
-   *   The subdivision repository.
-   *
-   * @SuppressWarnings(PHPMD.ExcessiveParameterList)
-   */
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, AddressFormatRepositoryInterface $address_format_repository, CountryRepositoryInterface $country_repository, SubdivisionRepositoryInterface $subdivision_repository) {
-    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
-
-    $this->addressFormatRepository = $address_format_repository;
-    $this->countryRepository = $country_repository;
-    $this->subdivisionRepository = $subdivision_repository;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    // @see \Drupal\Core\Field\FormatterPluginManager::createInstance().
-    return new static(
-      $plugin_id,
-      $plugin_definition,
-      $configuration['field_definition'],
-      $configuration['settings'],
-      $configuration['label'],
-      $configuration['view_mode'],
-      $configuration['third_party_settings'],
-      $container->get('address.address_format_repository'),
-      $container->get('address.country_repository'),
-      $container->get('address.subdivision_repository')
-    );
-  }
+class AddressInlineFormatter extends AddressDefaultFormatter {
 
   /**
    * {@inheritdoc}
@@ -184,7 +102,7 @@ class AddressInlineFormatter extends FormatterBase implements ContainerFactoryPl
       $format_string = $address_format->getFormat() . "\n" . '%country';
     }
 
-    $items = self::replacePlaceholders($format_string, $address_elements);
+    $items = $this->extractAddressItems($format_string, $address_elements);
 
     return [
       '#theme' => 'oe_theme_helper_address_inline',
@@ -200,17 +118,17 @@ class AddressInlineFormatter extends FormatterBase implements ContainerFactoryPl
   }
 
   /**
-   * Replaces placeholders in the given string.
+   * Extract address items from a format string and replaces placeholders.
    *
    * @param string $string
-   *   The string containing the placeholders.
+   *   The address format string, containing placeholders.
    * @param array $replacements
-   *   An array of replacements keyed by their placeholders.
+   *   An array of address items.
    *
    * @return array
    *   The exploded lines.
    */
-  public static function replacePlaceholders(string $string, array $replacements): array {
+  protected function extractAddressItems(string $string, array $replacements): array {
     // Make sure the replacements don't have any unneeded newlines.
     $replacements = array_map('trim', $replacements);
     $string = strtr($string, $replacements);
@@ -226,52 +144,6 @@ class AddressInlineFormatter extends FormatterBase implements ContainerFactoryPl
     $lines = array_filter($lines);
 
     return $lines;
-  }
-
-  /**
-   * Gets the address values used for rendering.
-   *
-   * @param \Drupal\address\AddressInterface $address
-   *   The address.
-   * @param \CommerceGuys\Addressing\AddressFormat\AddressFormat $address_format
-   *   The address format.
-   *
-   * @return array
-   *   The values, keyed by address field.
-   */
-  protected function getValues(AddressInterface $address, AddressFormat $address_format): array {
-    $values = [];
-    foreach (AddressField::getAll() as $field) {
-      $getter = 'get' . ucfirst($field);
-      $values[$field] = $address->$getter();
-    }
-
-    $original_values = [];
-    $subdivision_fields = $address_format->getUsedSubdivisionFields();
-    $parents = [];
-    foreach ($subdivision_fields as $index => $field) {
-      if (empty($values[$field])) {
-        // This level is empty, so there can be no sublevels.
-        break;
-      }
-      $parents[] = $index ? $original_values[$subdivision_fields[$index - 1]] : $address->getCountryCode();
-      $subdivision = $this->subdivisionRepository->get($values[$field], $parents);
-      if (!$subdivision) {
-        break;
-      }
-
-      // Remember the original value so that it can be used for $parents.
-      $original_values[$field] = $values[$field];
-      // Replace the value with the expected code.
-      $use_local_name = Locale::matchCandidates($address->getLocale(), $subdivision->getLocale());
-      $values[$field] = $use_local_name ? $subdivision->getLocalCode() : $subdivision->getCode();
-      if (!$subdivision->hasChildren()) {
-        // The current subdivision has no children, stop.
-        break;
-      }
-    }
-
-    return $values;
   }
 
 }
