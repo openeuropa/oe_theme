91282a5ef748e0b510f42e4c77412155a313ec4d
Antonio De Marco <antonio@nuvole.org>
2020-02-04 10:26:00 +0100

    OPENEUROPA-2221: Do not use lazy building in SummaryExtraField.

--- modules/oe_theme_content_event/src/Plugin/ExtraField/Display/SummaryExtraField.php
+++ modules/oe_theme_content_event/src/Plugin/ExtraField/Display/SummaryExtraField.php
@@ -4,10 +4,14 @@ declare(strict_types = 1);
 
 namespace Drupal\oe_theme_content_event\Plugin\ExtraField\Display;
 
+use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\extra_field\Plugin\ExtraFieldDisplayFormattedBase;
 use Drupal\oe_content_event\EventNodeWrapper;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Extra field displaying either the event summary or a report summary.
@@ -21,9 +25,56 @@ use Drupal\oe_content_event\EventNodeWrapper;
  *   visible = true
  * )
  */
-class SummaryExtraField extends ExtraFieldDisplayFormattedBase {
+class SummaryExtraField extends ExtraFieldDisplayFormattedBase implements ContainerFactoryPluginInterface {
 
   use StringTranslationTrait;
+
+  /**
+   * Entity view builder object.
+   *
+   * @var \Drupal\Core\Entity\EntityViewBuilderInterface
+   */
+  protected $viewBuilder;
+
+  /**
+   * Time service.
+   *
+   * @var \Drupal\Component\Datetime\TimeInterface
+   */
+  protected $time;
+
+  /**
+   * SummaryExtraField constructor.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   Entity view builder object.
+   * @param \Drupal\Component\Datetime\TimeInterface $time
+   *   Time service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, TimeInterface $time) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->viewBuilder = $entity_type_manager->getViewBuilder('node');
+    $this->time = $time;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('datetime.time')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -36,36 +87,17 @@ class SummaryExtraField extends ExtraFieldDisplayFormattedBase {
    * {@inheritdoc}
    */
   public function viewElements(ContentEntityInterface $entity) {
-    return [
-      '#lazy_builder' => [SummaryExtraField::class . '::lazyBuilder', [$entity->id()]],
-      '#create_placeholder' => TRUE,
-    ];
-  }
-
-  /**
-   * Lazy builder callback to conditionally render the event summary.
-   *
-   * @param string|int|null $id
-   *   Entity ID.
-   *
-   * @return array
-   *   Render array.
-   */
-  public static function lazyBuilder($id): array {
-    $node = \Drupal::entityTypeManager()->getStorage('node')->load($id);
-    $event = new EventNodeWrapper($node);
-    $current_time = \Drupal::time()->getRequestTime();
-    $now = (new \DateTime())->setTimestamp($current_time);
-    $view_builder = \Drupal::entityTypeManager()->getViewBuilder('node');
+    $event = new EventNodeWrapper($entity);
+    $now = (new \DateTime())->setTimestamp($this->time->getRequestTime());
 
     // Show description summary by default.
-    $renderable = $view_builder->viewField($node->get('oe_event_description_summary'), [
+    $renderable = $this->viewBuilder->viewField($entity->get('oe_event_description_summary'), [
       'label' => 'hidden',
     ]);
 
     // If the event is over and an event report summary is available, use that.
-    if ($event->isOver($now) && !$node->get('oe_event_report_summary')->isEmpty()) {
-      $renderable = $view_builder->viewField($node->get('oe_event_report_summary'), [
+    if ($event->isOver($now) && !$entity->get('oe_event_report_summary')->isEmpty()) {
+      $renderable = $this->viewBuilder->viewField($entity->get('oe_event_report_summary'), [
         'label' => 'hidden',
       ]);
     }
