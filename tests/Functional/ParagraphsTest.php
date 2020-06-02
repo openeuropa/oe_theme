a42a2cb7306728b55ea7230c19df4323219392bd
22Alexandra <beldean.ale@gmail.com>
2020-06-02 15:20:41 +0200

    OPENEUROPA-2068: Add test.

---
+++ tests/Functional/ParagraphsTest.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_theme\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests that theme configuration is correctly applied.
+ */
+class ParagraphsTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'config',
+    'system',
+    'node',
+    'oe_theme_helper',
+    'oe_paragraphs_demo',
+    'oe_content_timeline_field',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    // Enable OpenEuropa Theme.
+    $this->container->get('theme_installer')->install(['oe_theme']);
+
+    $user = $this->drupalCreateUser([], '', TRUE);
+    $this->drupalLogin($user);
+  }
+
+  /**
+   * Test Accordion item paragraph form.
+   */
+  public function testAccordionParagraph(): void {
+    $this->drupalGet('/node/add/oe_demo_landing_page');
+    $page = $this->getSession()->getPage();
+    $page->pressButton('Add Accordion');
+
+    // Assert the title and body fields of Accordion item paragraph are shown
+    // but the icon field is not.
+    $this->assertSession()->elementExists('css', '.field--name-field-oe-text');
+    $this->assertSession()->elementExists('css', '.field--name-field-oe-text-long');
+
+    $values = [
+      'title[0][value]' => 'Test Accordion',
+      'field_oe_demo_body[0][subform][field_oe_paragraphs][0][subform][field_oe_text][0][value]' => 'Accordion item title',
+      'field_oe_demo_body[0][subform][field_oe_paragraphs][0][subform][field_oe_text_long][0][value]' => 'Accordion item body',
+    ];
+    $this->drupalPostForm(NULL, $values, 'Save');
+    $this->drupalGet('/node/1');
+
+    // Assert paragraph values are displayed.
+    $this->assertSession()->pageTextContains('Accordion item title');
+    $this->assertSession()->pageTextContains('Accordion item body');
+  }
+
+}
