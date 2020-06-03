20daab7056c893b7776c6b19eb7224d050d4e378
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2020-06-03 18:15:37 +0200

    OPENEUROPA-3180: Inject Entity repository service.

--- modules/oe_theme_content_event/src/Plugin/ExtraField/Display/DetailsExtraField.php
+++ modules/oe_theme_content_event/src/Plugin/ExtraField/Display/DetailsExtraField.php
@@ -6,8 +6,11 @@ namespace Drupal\oe_theme_content_event\Plugin\ExtraField\Display;
 
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityRepositoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\oe_content_entity_venue\Entity\VenueInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Extra field displaying event details as a list of icons and text.
@@ -22,6 +25,45 @@ use Drupal\oe_content_entity_venue\Entity\VenueInterface;
  * )
  */
 class DetailsExtraField extends EventExtraFieldBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The entity repository.
+   *
+   * @var \Drupal\Core\Entity\EntityRepositoryInterface
+   */
+  protected $entityRepository;
+
+  /**
+   * EventExtraFieldBase constructor.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
+   *   The entity repository service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, EntityRepositoryInterface $entity_repository) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager);
+    $this->entityRepository = $entity_repository;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('entity.repository')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -119,11 +161,7 @@ class DetailsExtraField extends EventExtraFieldBase implements ContainerFactoryP
       return;
     }
 
-    // Inherit language from parent entity.
-    if ($venue->hasTranslation($entity->language()->getId())) {
-      $venue = $venue->getTranslation($entity->language()->getId());
-    }
-
+    $venue = $this->entityRepository->getTranslationFromContext($venue);
     $cacheability = CacheableMetadata::createFromRenderArray($build);
 
     $access = $venue->access('view', NULL, TRUE);
