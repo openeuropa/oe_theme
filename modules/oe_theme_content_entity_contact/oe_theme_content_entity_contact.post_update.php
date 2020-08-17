91845642a3ab8e4efc63a4cbfe2dcf60023a3288
Antonio De Marco <antonio@nuvole.org>
2020-08-17 14:47:28 +0200

    OPENEUROPA-3119: Update oe_details view mode, as it is already created by oe_content_entity_contact.

--- modules/oe_theme_content_entity_contact/oe_theme_content_entity_contact.post_update.php
+++ modules/oe_theme_content_entity_contact/oe_theme_content_entity_contact.post_update.php
@@ -11,26 +11,17 @@ use Drupal\Core\Entity\Entity\EntityViewDisplay;
 use Drupal\Core\Config\FileStorage;
 
 /**
- * Create "oe_details" Contact entity view displays.
+ * Update "Default" and "Details" Contact entity view displays.
  */
 function oe_theme_content_entity_contact_post_update_00001(): void {
-  // Create a file storage instance for reading configurations.
-  $storage = new FileStorage(drupal_get_path('module', 'oe_theme_content_entity_contact') . '/config/post_updates/00001_create_view_display');
-
-  // Create new configurations.
-  \Drupal::service('config.installer')->installOptionalConfig($storage);
-}
-
-/**
- * Update "default" Contact entity view display.
- */
-function oe_theme_content_entity_contact_post_update_00002(): void {
-  $storage = new FileStorage(drupal_get_path('module', 'oe_theme_content_entity_contact') . '/config/post_updates/00002_update_view_display');
+  $storage = new FileStorage(drupal_get_path('module', 'oe_theme_content_entity_contact') . '/config/post_updates/00001_update_view_display');
 
   // View display configurations to update.
   $displays = [
     'core.entity_view_display.oe_contact.oe_general.default',
+    'core.entity_view_display.oe_contact.oe_general.oe_details',
     'core.entity_view_display.oe_contact.oe_press.default',
+    'core.entity_view_display.oe_contact.oe_press.oe_details',
   ];
   foreach ($displays as $display) {
     $values = $storage->read($display);
