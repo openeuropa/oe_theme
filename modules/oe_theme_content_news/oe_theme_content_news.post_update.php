295ccdb448a86b2970f537e717ad0c4e7523ea4c
Antonio De Marco <antonio@nuvole.org>
2020-09-01 15:59:00 +0200

    EWPP-43: Take over teaser view display, regardless if it already exists or not.

--- modules/oe_theme_content_news/oe_theme_content_news.post_update.php
+++ modules/oe_theme_content_news/oe_theme_content_news.post_update.php
@@ -8,6 +8,8 @@
 declare(strict_types = 1);
 
 use Drupal\Core\Datetime\Entity\DateFormat;
+use Drupal\Core\Entity\Entity\EntityViewDisplay;
+use Drupal\Core\Config\FileStorage;
 
 /**
  * Add a date format for the News page header metadata.
@@ -16,4 +18,24 @@ function oe_theme_content_news_post_update_00001(array &$sandbox): void {
   $news_date_format = DateFormat::load('oe_theme_news_date');
   $news_date_format->set('pattern', 'j F Y');
   $news_date_format->save();
+}
+
+/**
+ * Override news teaser view display.
+ */
+function oe_theme_content_news_post_update_00002(): void {
+  $storage = new FileStorage(drupal_get_path('module', 'oe_theme_content_news') . '/config/post_updates/00002_override_teaser_view_display');
+  $display_values = $storage->read('core.entity_view_display.node.oe_news.teaser');
+  $storage = \Drupal::entityTypeManager()->getStorage('entity_view_display');
+
+  // Take over teaser view display, regardless if it already exists or not.
+  $view_display = EntityViewDisplay::load($display_values['id']);
+  if ($view_display) {
+    $display = $storage->updateFromStorageRecord($view_display, $display_values);
+    $display->save();
+    return;
+  }
+
+  $display = $storage->createFromStorageRecord($display_values);
+  $display->save();
 }
--- modules/oe_theme_content_publication/oe_theme_content_publication.post_update.php
+++ modules/oe_theme_content_publication/oe_theme_content_publication.post_update.php
@@ -8,6 +8,8 @@
 declare(strict_types = 1);
 
 use Drupal\Core\Datetime\Entity\DateFormat;
+use Drupal\Core\Entity\Entity\EntityViewDisplay;
+use Drupal\Core\Config\FileStorage;
 
 /**
  * Add a date format for the Publication page header metadata.
@@ -30,7 +32,27 @@ function oe_theme_content_publication_post_update_00001_add_publication_date_for
  * Update a date format for the Publication page header metadata.
  */
 function oe_theme_content_publication_post_update_00002(array &$sandbox): void {
-  $news_date_format = DateFormat::load('oe_theme_publication_date');
-  $news_date_format->set('pattern', 'j F Y');
-  $news_date_format->save();
+  $publication_date_format = DateFormat::load('oe_theme_publication_date');
+  $publication_date_format->set('pattern', 'j F Y');
+  $publication_date_format->save();
+}
+
+/**
+ * Override publication teaser view display.
+ */
+function oe_theme_content_publication_post_update_00003(): void {
+  $storage = new FileStorage(drupal_get_path('module', 'oe_theme_content_publication') . '/config/post_updates/00003_override_teaser_view_display');
+  $display_values = $storage->read('core.entity_view_display.node.oe_publication.teaser');
+  $storage = \Drupal::entityTypeManager()->getStorage('entity_view_display');
+
+  // Take over teaser view display, regardless if it already exists or not.
+  $view_display = EntityViewDisplay::load($display_values['id']);
+  if ($view_display) {
+    $display = $storage->updateFromStorageRecord($view_display, $display_values);
+    $display->save();
+    return;
+  }
+
+  $display = $storage->createFromStorageRecord($display_values);
+  $display->save();
 }
