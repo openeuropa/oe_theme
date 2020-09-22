d3d87dc9512fdf74fc98f2f5995e67cfba26779b
22Alexandra <beldean.ale@gmail.com>
2020-09-22 11:25:03 +0200

    EWPP-272: Add authors and location to page header.

--- modules/oe_theme_content_news/src/Plugin/PageHeaderMetadata/NewsContentType.php
+++ modules/oe_theme_content_news/src/Plugin/PageHeaderMetadata/NewsContentType.php
@@ -6,9 +6,10 @@ namespace Drupal\oe_theme_content_news\Plugin\PageHeaderMetadata;
 
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\Entity\EntityRepositoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\oe_theme_helper\Plugin\PageHeaderMetadata\NodeViewRoutesBase;
-use Drupal\rdf_skos\Entity\ConceptInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
@@ -24,6 +25,20 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 class NewsContentType extends NodeViewRoutesBase {
 
   use StringTranslationTrait;
+
+  /**
+   * The entity repository.
+   *
+   * @var \Drupal\Core\Entity\EntityRepositoryInterface
+   */
+  protected $entityRepository;
+
+  /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
 
   /**
    * The date formatter.
@@ -45,10 +60,16 @@ class NewsContentType extends NodeViewRoutesBase {
    *   The event dispatcher.
    * @param \Drupal\Core\Datetime\DateFormatterInterface $date_formatter
    *   The date formatter.
+   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
+   *   The entity repository service.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
    */
-  public function __construct(array $configuration, string $plugin_id, $plugin_definition, EventDispatcherInterface $event_dispatcher, DateFormatterInterface $date_formatter) {
+  public function __construct(array $configuration, string $plugin_id, $plugin_definition, EventDispatcherInterface $event_dispatcher, DateFormatterInterface $date_formatter, EntityRepositoryInterface $entity_repository, EntityTypeManagerInterface $entity_type_manager) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $event_dispatcher);
     $this->dateFormatter = $date_formatter;
+    $this->entityRepository = $entity_repository;
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
@@ -60,7 +81,9 @@ class NewsContentType extends NodeViewRoutesBase {
       $plugin_id,
       $plugin_definition,
       $container->get('event_dispatcher'),
-      $container->get('date.formatter')
+      $container->get('date.formatter'),
+      $container->get('entity.repository'),
+      $container->get('entity_type.manager')
     );
   }
 
@@ -100,17 +123,34 @@ class NewsContentType extends NodeViewRoutesBase {
       ];
     }
 
-    $news_type = $node->get('oe_news_types')->entity;
-    if ($news_type instanceof ConceptInterface) {
-      $cacheability = new CacheableMetadata();
+    $cacheability = new CacheableMetadata();
+
+    $news_types_labels = [];
+    $news_types = $node->get('oe_news_types')->getValue();
+    foreach ($news_types as $key => $news_type) {
+      $news_type = $this->entityTypeManager->getStorage('skos_concept')->load($news_type['target_id']);
       $cacheability->addCacheableDependency($news_type);
-      $news_type = \Drupal::service('entity.repository')->getTranslationFromContext($news_type);
-      $news_type_label = $news_type->label();
+      $news_type = $this->entityRepository->getTranslationFromContext($news_type);
+      $news_types_labels[$key] = $news_type->label();
     }
+
     $timestamp = $node->get('oe_publication_date')->date->getTimestamp();
+
+    $location = $node->get('oe_news_location')->entity;
+    $cacheability->addCacheableDependency($location);
+
+    $authors = $node->get('oe_author')->getValue();
+    foreach ($authors as $key => $author) {
+      $author = $this->entityTypeManager->getStorage('skos_concept')->load($author['target_id']);
+      $cacheability->addCacheableDependency($author);
+      $author = $this->entityRepository->getTranslationFromContext($author);
+      $authors_labels[$key] = $author->label();
+    }
     $metadata['metas'] = [
-      isset($news_type_label) ? $news_type_label : $this->t('News'),
+      isset($news_types_labels) ? $news_types_labels : $this->t('News'),
       $this->dateFormatter->format($timestamp, 'oe_theme_news_date'),
+      isset($location) ? $location->label() : '',
+      isset($authors_labels) ? $authors_labels : '',
     ];
 
     return $metadata;
