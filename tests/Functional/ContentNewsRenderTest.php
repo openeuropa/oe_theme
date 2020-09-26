6ccf09f0bd6182045ee03355560590f40c922e65
kaikiku <kaikiku@gmail.com>
2020-09-26 12:13:40 +0200

    D8TSDT-120: Add tests for contacts.

--- tests/Functional/ContentNewsRenderTest.php
+++ tests/Functional/ContentNewsRenderTest.php
@@ -4,7 +4,11 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\oe_theme\Functional;
 
+use Behat\Mink\Element\NodeElement;
 use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\media\MediaInterface;
+use Drupal\oe_content_entity\Entity\CorporateEntityInterface;
+use Drupal\oe_content_entity_contact\Entity\ContactInterface;
 use Drupal\Tests\BrowserTestBase;
 use Drupal\Tests\oe_theme\PatternAssertions\FieldListAssert;
 use Drupal\user\Entity\Role;
@@ -24,6 +28,7 @@ class ContentNewsRenderTest extends BrowserTestBase {
     'oe_theme_helper',
     'path',
     'oe_theme_content_news',
+    'oe_theme_content_entity_contact',
     'block',
   ];
 
@@ -42,6 +47,7 @@ class ContentNewsRenderTest extends BrowserTestBase {
 
     Role::load(RoleInterface::ANONYMOUS_ID)
       ->grantPermission('view published skos concept entities')
+      ->grantPermission('view published oe_contact')
       ->save();
   }
 
@@ -49,6 +55,10 @@ class ContentNewsRenderTest extends BrowserTestBase {
    * Tests that the News page renders correctly.
    */
   public function testNewsRendering(): void {
+    // Create general contact.
+    $general_contact = [
+      $this->createContactEntity('general_contact', 'oe_general', CorporateEntityInterface::PUBLISHED),
+    ];
     // Create a News node.
     /** @var \Drupal\node\Entity\Node $node */
     $node = $this->getStorage('node')->create([
@@ -61,6 +71,7 @@ class ContentNewsRenderTest extends BrowserTestBase {
       'oe_publication_date' => [
         'value' => '2020-09-18',
       ],
+      'oe_news_contacts' => $general_contact,
       'oe_author' => 'http://publications.europa.eu/resource/authority/corporate-body/ACJHR',
       'oe_news_location' => 'http://publications.europa.eu/resource/authority/place/ARE_AUH',
       'oe_content_content_owner' => 'http://publications.europa.eu/resource/authority/corporate-body/COMMU',
@@ -69,6 +80,68 @@ class ContentNewsRenderTest extends BrowserTestBase {
     ]);
     $node->save();
     $this->drupalGet($node->toUrl());
+
+    // Assert news contacts.
+    $contacts = $this->assertSession()->elementExists('css', 'div#news-contacts');
+
+    $contact_headers = $contacts->findAll('css', 'h2');
+    $this->assertEquals('Contacts', $contact_headers[0]->getText());
+
+    $contact_name = $contacts->findAll('css', 'h3');
+    $this->assertCount(1, $contact_name);
+    $this->assertEquals('general_contact', $contact_name[0]->getText());
+
+    $contact_body = $contacts->findAll('css', '.ecl-editor');
+    $this->assertCount(1, $contact_body);
+    $this->assertEquals('Body text general_contact', $contact_body[0]->getText());
+
+    $contacts_html = $contacts->getHtml();
+    $field_list_assert = new FieldListAssert();
+    $contact_expected_values = [
+      'items' => [
+        [
+          'label' => 'Organisation',
+          'body' => 'Organisation general_contact',
+        ], [
+          'label' => 'Website',
+          'body' => 'http://www.example.com/website_general_contact',
+        ], [
+          'label' => 'Email',
+          'body' => 'general_contact@example.com',
+        ], [
+          'label' => 'Phone number',
+          'body' => 'Phone number general_contact',
+        ], [
+          'label' => 'Mobile number',
+          'body' => 'Mobile number general_contact',
+        ], [
+          'label' => 'Fax number',
+          'body' => 'Fax number general_contact',
+        ], [
+          'label' => 'Postal address',
+          'body' => 'Address general_contact, 1001 Brussels, Belgium',
+        ], [
+          'label' => 'Office',
+          'body' => 'Office general_contact',
+        ], [
+          'label' => 'Social media',
+          'body' => 'Social media general_contact',
+        ],
+      ],
+    ];
+    $field_list_assert->assertPattern($contact_expected_values, $contacts_html);
+    $field_list_assert->assertVariant('horizontal', $contacts_html);
+    // Assert Press contacts.
+    $press = $contacts->findAll('.ecl-u-border-top.ecl-u-border-bottom.ecl-u-border-color-grey-15.ecl-u-mt-s.ecl-u-pt-l.ecl-u-pb-l');
+    $press_link = $press->findAll('a');
+    $this->assertEquals('http://www.example.com/press_contact_general_contact', $press_link->attr('href'));
+    $press_label = $press_link->findAll('.ecl-link__label');
+    $this->assertCount(1, $press_label);
+    $this->assertEquals('Press contacts', $press_label->attr('href'));
+    $press_icon = $press_link->findAll('ecl-icon.ecl-icon--s.ecl-icon--primary.ecl-link__icon');
+    $this->assertCount(1, $press_icon);
+    // Assert contacts Image.
+    $this->assertFeaturedMediaField($contacts, 'general_contact');
 
     // Assert page header - metadata.
     $this->assertSession()->elementTextContains('css', '.ecl-page-header-core .ecl-page-header-core__title', 'Test news node');
@@ -125,6 +198,112 @@ class ContentNewsRenderTest extends BrowserTestBase {
    */
   protected function getStorage(string $entity_type_id): EntityStorageInterface {
     return \Drupal::entityTypeManager()->getStorage($entity_type_id);
+  }
+
+  /**
+   * Creates media image entity.
+   *
+   * @param string $name
+   *   Name of the image media.
+   *
+   * @return \Drupal\media\MediaInterface
+   *   Media image instance.
+   */
+  protected function createMediaImage(string $name): MediaInterface {
+    // Create file instance.
+    $file = file_save_data(file_get_contents(drupal_get_path('theme', 'oe_theme') . '/tests/fixtures/placeholder.png'), "public://placeholder_$name.png");
+    $file->setPermanent();
+    $file->save();
+
+    $media = $this->getStorage('media')->create([
+      'bundle' => 'image',
+      'name' => "Test image $name",
+      'oe_media_image' => [
+        'target_id' => (int) $file->id(),
+        'alt' => "Alternative text $name",
+      ],
+      'uid' => 0,
+      'status' => 1,
+    ]);
+    $media->save();
+
+    return $media;
+  }
+
+  /**
+   * Creates Contact entity.
+   *
+   * @param string $name
+   *   Entity name. Is used as a parameter for test data.
+   * @param string $bundle
+   *   Entity bundle.
+   * @param int $status
+   *   Entity status.
+   *
+   * @return \Drupal\oe_content_entity_contact\Entity\ContactInterface
+   *   Contact entity.
+   */
+  protected function createContactEntity(string $name, string $bundle, int $status): ContactInterface {
+    // Create image for contact.
+    $media = $this->createMediaImage($name);
+
+    $contact = $this->getStorage('oe_contact')->create([
+      'bundle' => $bundle,
+      'name' => $name,
+      'oe_address' => [
+        'country_code' => 'BE',
+        'locality' => 'Brussels',
+        'address_line1' => "Address $name",
+        'postal_code' => '1001',
+      ],
+      'oe_body' => "Body text $name",
+      'oe_email' => "$name@example.com",
+      'oe_fax' => "Fax number $name",
+      'oe_mobile' => "Mobile number $name",
+      'oe_office' => "Office $name",
+      'oe_organisation' => "Organisation $name",
+      'oe_phone' => "Phone number $name",
+      'oe_press_contact_url' => ['uri' => "http://www.example.com/press_contact_$name"],
+      'oe_social_media' => [
+        [
+          'uri' => "http://www.example.com/social_media_$name",
+          'title' => "Social media $name",
+          'link_type' => 'facebook',
+        ],
+      ],
+      'oe_website' => ['uri' => "http://www.example.com/website_$name"],
+      'oe_image' => [
+        [
+          'target_id' => (int) $media->id(),
+          'caption' => "Caption $name",
+        ],
+      ],
+      'status' => $status,
+    ]);
+
+    return $contact;
+  }
+
+  /**
+   * Asserts featured media field rendering.
+   *
+   * @param \Behat\Mink\Element\NodeElement $rendered_element
+   *   Rendered element.
+   * @param string $name
+   *   Name of the image media.
+   */
+  protected function assertFeaturedMediaField(NodeElement $rendered_element, string $name): void {
+    $figures = $rendered_element->findAll('css', 'figure.ecl-media-container');
+    $this->assertCount(1, $figures);
+
+    // Assert image tag.
+    $image = $figures[0]->find('css', 'img');
+    $this->assertContains("placeholder_$name.png", $image->getAttribute('src'));
+    $this->assertEquals("Alternative text $name", $image->getAttribute('alt'));
+
+    // Assert caption.
+    $caption = $figures[0]->find('css', 'figcaption');
+    $this->assertEquals("Caption $name", $caption->getText());
   }
 
 }
