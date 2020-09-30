ee3f62c31609b6011c49630e2e57a462007fb6e0
drishu <aszilagyi@live.com>
2020-09-30 12:15:30 +0000

    EWPP-328: Theme date and datepicker using ecl.

--- ecl-builder.config.js
+++ ecl-builder.config.js
@@ -64,6 +64,7 @@ module.exports = {
     { from: path.resolve(nodeModules, '@ecl-twig'), patterns: 'ec-component-tag/*.twig', to: path.resolve(outputFolder, 'components') },
     { from: path.resolve(nodeModules, '@ecl-twig'), patterns: 'ec-component-text-input/*.twig', to: path.resolve(outputFolder, 'components') },
     { from: path.resolve(nodeModules, '@ecl-twig'), patterns: 'ec-component-timeline/*.twig', to: path.resolve(outputFolder, 'components') },
+    { from: path.resolve(nodeModules, '@ecl-twig'), patterns: 'ec-component-datepicker/*.twig', to: path.resolve(outputFolder, 'components') },
     { from: path.resolve(nodeModules, 'svg4everybody/dist'), patterns: 'svg4everybody.min.js', to: path.resolve(outputFolder, 'dist/js') },
   ]
 };
