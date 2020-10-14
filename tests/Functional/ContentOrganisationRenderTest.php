ac132ca1d5781c9fee5eb49aef614ef3fb2a0745
Evgenii Nikitin <yenyasinn@gmail.com>
2020-10-14 15:51:19 +0300

    EWPP-402: Rework of contact entity assertion.

--- tests/Functional/ContentOrganisationRenderTest.php
+++ tests/Functional/ContentOrganisationRenderTest.php
@@ -4,12 +4,6 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\oe_theme\Functional;
 
-use Behat\Mink\Element\NodeElement;
-use Drupal\Core\Entity\EntityStorageInterface;
-use Drupal\media\MediaInterface;
-use Drupal\oe_content_entity\Entity\CorporateEntityInterface;
-use Drupal\oe_content_entity_contact\Entity\ContactInterface;
-use Drupal\Tests\BrowserTestBase;
 use Drupal\user\Entity\Role;
 use Drupal\user\RoleInterface;
 use Drupal\Tests\oe_theme\PatternAssertions\PatternPageHeaderAssert;
@@ -19,7 +13,7 @@ use Drupal\media\Entity\Media;
 /**
  * Tests organisation (oe_organisation) content type render.
  */
-class ContentOrganisationRenderTest extends BrowserTestBase {
+class ContentOrganisationRenderTest extends ContentRenderTestBase {
 
   /**
    * {@inheritdoc}
@@ -42,17 +36,6 @@ class ContentOrganisationRenderTest extends BrowserTestBase {
    */
   protected function setUp(): void {
     parent::setUp();
-
-    // Enable and set OpenEuropa Theme as default.
-    \Drupal::service('theme_installer')->install(['oe_theme']);
-    \Drupal::configFactory()
-      ->getEditable('system.theme')
-      ->set('default', 'oe_theme')
-      ->save();
-
-    // Rebuild the ui_pattern definitions to collect the ones provided by
-    // oe_theme itself.
-    \Drupal::service('plugin.manager.ui_patterns')->clearCachedDefinitions();
 
     // Give anonymous users permission to view entities.
     Role::load(RoleInterface::ANONYMOUS_ID)
@@ -80,8 +63,6 @@ class ContentOrganisationRenderTest extends BrowserTestBase {
       'status' => 1,
     ]);
     $media->save();
-
-    $general_contact = $this->createContactEntity('general_contact', 'oe_general', CorporateEntityInterface::PUBLISHED);
 
     /** @var \Drupal\node\Entity\Node $node */
     $node = $this->getStorage('node')->create([
@@ -127,21 +108,19 @@ class ContentOrganisationRenderTest extends BrowserTestBase {
 
     // Add body text and contact values.
     $node->set('body', 'My body text');
-    $node->set('oe_organisation_contact', [$general_contact]);
     $node->save();
     $this->drupalGet($node->toUrl());
 
     // Assert navigation part.
     $navigation = $this->assertSession()->elementExists('css', 'nav.ecl-inpage-navigation');
-    $assert = new InPageNavigationAssert();
-    $expected_values = [
+    $inpage_nav_assert = new InPageNavigationAssert();
+    $inpage_nav_expected_values = [
       'title' => 'Page contents',
       'list' => [
         ['label' => 'Description', 'href' => '#description'],
-        ['label' => 'Contact', 'href' => '#contact'],
       ],
     ];
-    $assert->assertPattern($expected_values, $navigation->getOuterHtml());
+    $inpage_nav_assert->assertPattern($inpage_nav_expected_values, $navigation->getOuterHtml());
 
     // Change logo to use av portal image.
     $media = Media::create([
@@ -169,7 +148,7 @@ class ContentOrganisationRenderTest extends BrowserTestBase {
     $content = $this->assertSession()->elementExists('css', '.ecl-col-lg-9', $wrapper);
     $this->assertSession()->elementsCount('css', '.ecl-col-lg-9', 1);
     $content_items = $content->findAll('xpath', '/div');
-    $this->assertCount(2, $content_items);
+    $this->assertCount(1, $content_items);
 
     // Assert header of first field group.
     $this->assertContentHeader($content_items[0], 'Description', 'description');
@@ -179,214 +158,17 @@ class ContentOrganisationRenderTest extends BrowserTestBase {
     $this->assertCount(1, $body);
     $this->assertEquals('My body text', $body[0]->getText());
 
-    // Assert header of second field group.
-    $this->assertContentHeader($content_items[1], 'Contact', 'contact');
-
     // Assert Organisation's contacts.
-    $contact_headers = $content_items[1]->findAll('css', 'h2');
-    $this->assertEquals($contact_headers[0]->getText(), 'Contact');
-    $this->assertContactRendering($content_items[1], 'general_contact');
-  }
+    $this->assertContactEntityDefaultDisplay($node, 'oe_organisation_contact');
 
-  /**
-   * Asserts field group header.
-   *
-   * @param \Behat\Mink\Element\NodeElement $element
-   *   Field group content.
-   * @param string $title
-   *   Expected title.
-   * @param string $id
-   *   Expected id.
-   */
-  protected function assertContentHeader(NodeElement $element, string $title, string $id): void {
-    $header = $element->find('css', 'h2.ecl-u-type-heading-2');
-    $this->assertEquals($title, $header->getText());
-    $this->assertEquals($id, $header->getAttribute('id'));
-  }
+    // Assert navigation part.
+    $inpage_nav_expected_values['list'][] = ['label' => 'Contact', 'href' => '#contact'];
+    $inpage_nav_assert->assertPattern($inpage_nav_expected_values, $navigation->getOuterHtml());
 
-  /**
-   * Creates Contact entity.
-   *
-   * @param string $name
-   *   Entity name. Is used as a parameter for test data.
-   * @param string $bundle
-   *   Entity bundle.
-   * @param int $status
-   *   Entity status.
-   *
-   * @return \Drupal\oe_content_entity_contact\Entity\ContactInterface
-   *   Contact entity.
-   */
-  protected function createContactEntity(string $name, string $bundle, int $status): ContactInterface {
-    // Create image for contact.
-    $media = $this->createMediaImage($name);
-
-    $contact = $this->getStorage('oe_contact')->create([
-      'bundle' => $bundle,
-      'name' => $name,
-      'oe_address' => [
-        'country_code' => 'BE',
-        'locality' => 'Brussels',
-        'address_line1' => "Address $name",
-        'postal_code' => '1001',
-      ],
-      'oe_body' => "Body text $name",
-      'oe_email' => "$name@example.com",
-      'oe_fax' => "Fax number $name",
-      'oe_mobile' => "Mobile number $name",
-      'oe_office' => "Office $name",
-      'oe_organisation' => "Organisation $name",
-      'oe_phone' => "Phone number $name",
-      'oe_press_contact_url' => ['uri' => "http://www.example.com/press_contact_$name"],
-      'oe_social_media' => [
-        [
-          'uri' => "http://www.example.com/social_media_$name",
-          'title' => "Social media $name",
-          'link_type' => 'facebook',
-        ],
-      ],
-      'oe_website' => ['uri' => "http://www.example.com/website_$name"],
-      'oe_image' => [
-        [
-          'target_id' => (int) $media->id(),
-          'caption' => "Caption $name",
-        ],
-      ],
-      'status' => $status,
-    ]);
-
-    return $contact;
-  }
-
-  /**
-   * Asserts rendering of Contact entity.
-   *
-   * @param \Behat\Mink\Element\NodeElement $rendered_element
-   *   Rendered element.
-   * @param string $name
-   *   Name of the entity.
-   */
-  protected function assertContactRendering(NodeElement $rendered_element, string $name): void {
-    $contact_sub_headers = $rendered_element->findAll('css', 'h3');
-    $this->assertCount(1, $contact_sub_headers);
-    $this->assertEquals($contact_sub_headers[0]->getText(), 'general_contact');
-
-    // Body field.
-    $body = $rendered_element->findAll('css', '.ecl-editor');
-    $this->assertCount(1, $body);
-    $this->assertEquals("Body text $name", $body[0]->getText());
-
-    // Assert list of fields in field_list pattern.
-    $description_lists = $rendered_element->findAll('css', 'dl.ecl-description-list.ecl-description-list--horizontal');
-    $this->assertCount(1, $description_lists);
-
-    // Assert labels in list of fields.
-    $description_list_labels = $description_lists[0]->findAll('css', 'dt.ecl-description-list__term');
-    $this->assertCount(9, $description_list_labels);
-    $labels = [
-      'Organisation',
-      'Website',
-      'Email',
-      'Phone number',
-      'Mobile number',
-      'Fax number',
-      'Postal address',
-      'Office',
-      'Social media',
-    ];
-    foreach ($labels as $key => $label) {
-      $this->assertEquals($label, $description_list_labels[$key]->getText());
-    }
-
-    // Assert values in list of fields.
-    $values = $description_lists[0]->findAll('css', 'dd.ecl-description-list__definition');
-    $this->assertCount(9, $values);
-    $this->assertEquals("Organisation $name", $values[0]->getText());
-
-    $values[1]->hasLink("http://www.example.com/website_$name");
-    $values[2]->hasLink("$name@example.com");
-    $this->assertEquals("Phone number $name", $values[3]->getText());
-    $this->assertEquals("Mobile number $name", $values[4]->getText());
-    $this->assertEquals("Fax number $name", $values[5]->getText());
-    $this->assertEquals("Address $name, 1001 Brussels, Belgium", $values[6]->getText());
-    $this->assertEquals("Office $name", $values[7]->getText());
-
-    // Assert social media link.
-    $social_media_links = $values[8]->findAll('css', '.ecl-link');
-    $this->assertCount(1, $social_media_links);
-    $social_media_link_label = $social_media_links[0]->find('css', '.ecl-link__label');
-    $this->assertEqual("Social media $name", $social_media_link_label->getText());
-    $this->assertContains("http://www.example.com/social_media_$name", $social_media_links[0]->getAttribute('href'));
-    $social_media_icon = $social_media_links[0]->find('css', 'use');
-    $this->assertContains('facebook', $social_media_icon->getAttribute('xlink:href'));
-
-    // Assert image.
-    $this->assertFeaturedMediaField($rendered_element, $name);
-  }
-
-  /**
-   * Gets the entity type's storage.
-   *
-   * @param string $entity_type_id
-   *   The entity type ID to get a storage for.
-   *
-   * @return \Drupal\Core\Entity\EntityStorageInterface
-   *   The entity type's storage.
-   */
-  protected function getStorage(string $entity_type_id): EntityStorageInterface {
-    return \Drupal::entityTypeManager()->getStorage($entity_type_id);
-  }
-
-  /**
-   * Creates media image entity.
-   *
-   * @param string $name
-   *   Name of the image media.
-   *
-   * @return \Drupal\media\MediaInterface
-   *   Media image instance.
-   */
-  protected function createMediaImage(string $name): MediaInterface {
-    // Create file instance.
-    $file = file_save_data(file_get_contents(drupal_get_path('theme', 'oe_theme') . '/tests/fixtures/placeholder.png'), "public://placeholder_$name.png");
-    $file->setPermanent();
-    $file->save();
-
-    $media = $this->getStorage('media')->create([
-      'bundle' => 'image',
-      'name' => "Test image $name",
-      'oe_media_image' => [
-        'target_id' => (int) $file->id(),
-        'alt' => "Alternative text $name",
-      ],
-      'uid' => 0,
-      'status' => 1,
-    ]);
-    $media->save();
-
-    return $media;
-  }
-
-  /**
-   * Asserts featured media field rendering.
-   *
-   * @param \Behat\Mink\Element\NodeElement $rendered_element
-   *   Rendered element.
-   * @param string $name
-   *   Name of the image media.
-   */
-  protected function assertFeaturedMediaField(NodeElement $rendered_element, string $name): void {
-    $figures = $rendered_element->findAll('css', 'figure.ecl-media-container');
-    $this->assertCount(1, $figures);
-
-    // Assert image tag.
-    $image = $figures[0]->find('css', 'img');
-    $this->assertContains("placeholder_$name.png", $image->getAttribute('src'));
-    $this->assertEquals("Alternative text $name", $image->getAttribute('alt'));
-
-    // Assert caption.
-    $caption = $figures[0]->find('css', 'figcaption');
-    $this->assertEquals("Caption $name", $caption->getText());
+    // Assert header of the second field group.
+    $content_items = $content->findAll('xpath', '/div');
+    $this->assertCount(2, $content_items);
+    $this->assertContentHeader($content_items[1], 'Contact', 'contact');
   }
 
 }
--- tests/Functional/ContentProjectRenderTest.php
+++ tests/Functional/ContentProjectRenderTest.php
@@ -5,19 +5,16 @@ declare(strict_types = 1);
 namespace Drupal\Tests\oe_theme\Functional;
 
 use Behat\Mink\Element\NodeElement;
-use Drupal\Core\Entity\EntityStorageInterface;
-use Drupal\media\MediaInterface;
 use Drupal\oe_content_entity\Entity\CorporateEntityInterface;
-use Drupal\oe_content_entity_contact\Entity\ContactInterface;
 use Drupal\oe_content_entity_organisation\Entity\OrganisationInterface;
-use Drupal\Tests\BrowserTestBase;
+use Drupal\Tests\oe_theme\PatternAssertions\PatternPageHeaderAssert;
 use Drupal\user\Entity\Role;
 use Drupal\user\RoleInterface;
 
 /**
  * Tests that our Project content type renders correctly.
  */
-class ContentProjectRenderTest extends BrowserTestBase {
+class ContentProjectRenderTest extends ContentRenderTestBase {
 
   /**
    * {@inheritdoc}
@@ -36,15 +33,8 @@ class ContentProjectRenderTest extends BrowserTestBase {
   /**
    * {@inheritdoc}
    */
-  protected function setUp() {
+  protected function setUp(): void {
     parent::setUp();
-
-    // Enable and set OpenEuropa Theme as default.
-    \Drupal::service('theme_installer')->install(['oe_theme']);
-    \Drupal::configFactory()->getEditable('system.theme')->set('default', 'oe_theme')->save();
-    // Rebuild the ui_pattern definitions to collect the ones provided by
-    // oe_theme itself.
-    \Drupal::service('plugin.manager.ui_patterns')->clearCachedDefinitions();
 
     // Give anonymous users permission to view organisation entities.
     Role::load(RoleInterface::ANONYMOUS_ID)
@@ -71,9 +61,6 @@ class ContentProjectRenderTest extends BrowserTestBase {
     // Unpublished entity should not be shown.
     $coordinator_organisation = $this->createStakeholderOrganisationEntity('coordinator', CorporateEntityInterface::PUBLISHED);
     $participant_organisation = $this->createStakeholderOrganisationEntity('participant', CorporateEntityInterface::NOT_PUBLISHED);
-
-    // Create general contact.
-    $general_contact = $this->createContactEntity('general_contact', 'oe_general', CorporateEntityInterface::PUBLISHED);
 
     // Create a Project node.
     /** @var \Drupal\node\Entity\Node $node */
@@ -132,7 +119,6 @@ class ContentProjectRenderTest extends BrowserTestBase {
       'oe_project_funding_programme' => 'http://publications.europa.eu/resource/authority/eu-programme/AFIS2020',
       'oe_project_coordinators' => [$coordinator_organisation],
       'oe_project_participants' => [$participant_organisation],
-      'oe_project_contact' => [$general_contact],
       'oe_documents' => [
         [
           'target_id' => (int) $media_project_document->id(),
@@ -146,8 +132,14 @@ class ContentProjectRenderTest extends BrowserTestBase {
     $this->drupalGet($node->toUrl());
 
     // Assert page header - metadata.
-    $this->assertSession()->elementTextContains('css', '.ecl-page-header-core .ecl-page-header-core__meta', 'Project');
-    $this->assertSession()->elementTextContains('css', '.ecl-page-header-core .ecl-page-header-core__description', 'Summary');
+    $page_header = $this->assertSession()->elementExists('css', '.ecl-page-header-core');
+    $assert = new PatternPageHeaderAssert();
+    $expected_values = [
+      'title' => 'Test project node',
+      'description' => 'Summary',
+      'meta' => 'Project',
+    ];
+    $assert->assertPattern($expected_values, $page_header->getOuterHtml());
 
     // Assert top region - Project details.
     $project_details = $this->assertSession()->elementExists('css', 'div#project-details');
@@ -301,12 +293,12 @@ class ContentProjectRenderTest extends BrowserTestBase {
     $this->assertStakeholderOrganisationRendering($project_stakeholders, 'participant');
 
     // Assert Project's contacts.
+    $this->assertContactEntityDefaultDisplay($node, 'oe_project_contact');
     $project_contacts = $this->assertSession()->elementExists('css', 'div#project-contacts');
-    $contact_headers = $project_contacts->findAll('css', 'h2');
-    $this->assertEquals($contact_headers[0]->getText(), 'Contact');
-    $this->assertContactRendering($project_contacts, 'general_contact');
+    $this->assertContentHeader($project_contacts, 'Contact');
 
     // Unpublish Contact entity to test its visibility.
+    $general_contact = $node->get('oe_project_contact')->entity;
     $general_contact->set('status', CorporateEntityInterface::NOT_PUBLISHED);
     $general_contact->save();
 
@@ -357,60 +349,6 @@ class ContentProjectRenderTest extends BrowserTestBase {
   }
 
   /**
-   * Creates Contact entity.
-   *
-   * @param string $name
-   *   Entity name. Is used as a parameter for test data.
-   * @param string $bundle
-   *   Entity bundle.
-   * @param int $status
-   *   Entity status.
-   *
-   * @return \Drupal\oe_content_entity_contact\Entity\ContactInterface
-   *   Contact entity.
-   */
-  protected function createContactEntity(string $name, string $bundle, int $status): ContactInterface {
-    // Create image for contact.
-    $media = $this->createMediaImage($name);
-
-    $contact = $this->getStorage('oe_contact')->create([
-      'bundle' => $bundle,
-      'name' => $name,
-      'oe_address' => [
-        'country_code' => 'BE',
-        'locality' => 'Brussels',
-        'address_line1' => "Address $name",
-        'postal_code' => '1001',
-      ],
-      'oe_body' => "Body text $name",
-      'oe_email' => "$name@example.com",
-      'oe_fax' => "Fax number $name",
-      'oe_mobile' => "Mobile number $name",
-      'oe_office' => "Office $name",
-      'oe_organisation' => "Organisation $name",
-      'oe_phone' => "Phone number $name",
-      'oe_press_contact_url' => ['uri' => "http://www.example.com/press_contact_$name"],
-      'oe_social_media' => [
-        [
-          'uri' => "http://www.example.com/social_media_$name",
-          'title' => "Social media $name",
-          'link_type' => 'facebook',
-        ],
-      ],
-      'oe_website' => ['uri' => "http://www.example.com/website_$name"],
-      'oe_image' => [
-        [
-          'target_id' => (int) $media->id(),
-          'caption' => "Caption $name",
-        ],
-      ],
-      'status' => $status,
-    ]);
-
-    return $contact;
-  }
-
-  /**
    * Asserts rendering of a Stakeholder.
    *
    * @param \Behat\Mink\Element\NodeElement $rendered_stakeholder_element
@@ -446,167 +384,6 @@ class ContentProjectRenderTest extends BrowserTestBase {
     $contact_link_labels = $rendered_stakeholder_element->findAll('css', '.ecl-link__label');
     $this->assertCount(1, $contact_link_labels);
     $this->assertEquals('Contact organisation', $contact_link_labels[0]->getText());
-  }
-
-  /**
-   * Asserts rendering of Contact entity.
-   *
-   * @param \Behat\Mink\Element\NodeElement $rendered_element
-   *   Rendered element.
-   * @param string $name
-   *   Name of the entity.
-   */
-  protected function assertContactRendering(NodeElement $rendered_element, string $name): void {
-    $contact_sub_headers = $rendered_element->findAll('css', 'h3');
-    $this->assertCount(1, $contact_sub_headers);
-    $this->assertEquals($contact_sub_headers[0]->getText(), 'general_contact');
-
-    // Body field.
-    $body = $rendered_element->findAll('css', '.ecl-editor');
-    $this->assertCount(1, $body);
-    $this->assertEquals("Body text $name", $body[0]->getText());
-
-    // Assert list of fields in field_list pattern.
-    $description_lists = $rendered_element->findAll('css', 'dl.ecl-description-list.ecl-description-list--horizontal');
-    $this->assertCount(1, $description_lists);
-
-    // Assert labels in list of fields.
-    $description_list_labels = $description_lists[0]->findAll('css', 'dt.ecl-description-list__term');
-    $this->assertCount(9, $description_list_labels);
-    $labels = [
-      'Organisation',
-      'Website',
-      'Email',
-      'Phone number',
-      'Mobile number',
-      'Fax number',
-      'Postal address',
-      'Office',
-      'Social media',
-    ];
-    foreach ($labels as $key => $label) {
-      $this->assertEquals($label, $description_list_labels[$key]->getText());
-    }
-
-    // Assert values in list of fields.
-    $values = $description_lists[0]->findAll('css', 'dd.ecl-description-list__definition');
-    $this->assertCount(9, $values);
-    $this->assertEquals("Organisation $name", $values[0]->getText());
-
-    $values[1]->hasLink("http://www.example.com/website_$name");
-    $values[2]->hasLink("$name@example.com");
-    $this->assertEquals("Phone number $name", $values[3]->getText());
-    $this->assertEquals("Mobile number $name", $values[4]->getText());
-    $this->assertEquals("Fax number $name", $values[5]->getText());
-    $this->assertEquals("Address $name, 1001 Brussels, Belgium", $values[6]->getText());
-    $this->assertEquals("Office $name", $values[7]->getText());
-
-    // Assert social media link.
-    $social_media_links = $values[8]->findAll('css', '.ecl-link');
-    $this->assertCount(1, $social_media_links);
-    $social_media_link_label = $social_media_links[0]->find('css', '.ecl-link__label');
-    $this->assertEqual("Social media $name", $social_media_link_label->getText());
-    $this->assertContains("http://www.example.com/social_media_$name", $social_media_links[0]->getAttribute('href'));
-    $social_media_icon = $social_media_links[0]->find('css', 'use');
-    $this->assertContains('facebook', $social_media_icon->getAttribute('xlink:href'));
-
-    // Assert image.
-    $this->assertFeaturedMediaField($rendered_element, $name);
-  }
-
-  /**
-   * Gets the entity type's storage.
-   *
-   * @param string $entity_type_id
-   *   The entity type ID to get a storage for.
-   *
-   * @return \Drupal\Core\Entity\EntityStorageInterface
-   *   The entity type's storage.
-   */
-  protected function getStorage(string $entity_type_id): EntityStorageInterface {
-    return \Drupal::entityTypeManager()->getStorage($entity_type_id);
-  }
-
-  /**
-   * Creates media image entity.
-   *
-   * @param string $name
-   *   Name of the image media.
-   *
-   * @return \Drupal\media\MediaInterface
-   *   Media image instance.
-   */
-  protected function createMediaImage(string $name): MediaInterface {
-    // Create file instance.
-    $file = file_save_data(file_get_contents(drupal_get_path('theme', 'oe_theme') . '/tests/fixtures/placeholder.png'), "public://placeholder_$name.png");
-    $file->setPermanent();
-    $file->save();
-
-    $media = $this->getStorage('media')->create([
-      'bundle' => 'image',
-      'name' => "Test image $name",
-      'oe_media_image' => [
-        'target_id' => (int) $file->id(),
-        'alt' => "Alternative text $name",
-      ],
-      'uid' => 0,
-      'status' => 1,
-    ]);
-    $media->save();
-
-    return $media;
-  }
-
-  /**
-   * Creates media document entity.
-   *
-   * @param string $name
-   *   Name of the document media.
-   *
-   * @return \Drupal\media\MediaInterface
-   *   Media document instance.
-   */
-  protected function createMediaDocument(string $name): MediaInterface {
-    // Create file instance.
-    $file = file_save_data(file_get_contents(drupal_get_path('module', 'oe_media') . '/tests/fixtures/sample.pdf'), "public://sample_$name.pdf");
-    $file->setPermanent();
-    $file->save();
-
-    $media = $this->getStorage('media')->create([
-      'bundle' => 'document',
-      'name' => "Test document $name",
-      'oe_media_file_type' => 'local',
-      'oe_media_file' => [
-        'target_id' => (int) $file->id(),
-      ],
-      'uid' => 0,
-      'status' => 1,
-    ]);
-    $media->save();
-
-    return $media;
-  }
-
-  /**
-   * Asserts featured media field rendering.
-   *
-   * @param \Behat\Mink\Element\NodeElement $rendered_element
-   *   Rendered element.
-   * @param string $name
-   *   Name of the image media.
-   */
-  protected function assertFeaturedMediaField(NodeElement $rendered_element, string $name): void {
-    $figures = $rendered_element->findAll('css', 'figure.ecl-media-container');
-    $this->assertCount(1, $figures);
-
-    // Assert image tag.
-    $image = $figures[0]->find('css', 'img');
-    $this->assertContains("placeholder_$name.png", $image->getAttribute('src'));
-    $this->assertEquals("Alternative text $name", $image->getAttribute('alt'));
-
-    // Assert caption.
-    $caption = $figures[0]->find('css', 'figcaption');
-    $this->assertEquals("Caption $name", $caption->getText());
   }
 
 }
