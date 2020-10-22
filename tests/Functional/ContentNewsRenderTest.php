7191ecd7ef828e1ffce377719de6b3742d4c3206
Evgenii Nikitin <yenyasinn@gmail.com>
2020-10-22 16:57:02 +0300

    EWPP-402: Add new test cases to ContentNewsRenderTest.

--- tests/Functional/ContentNewsRenderTest.php
+++ tests/Functional/ContentNewsRenderTest.php
@@ -47,22 +47,15 @@ class ContentNewsRenderTest extends ContentRenderTestBase {
     $node = $this->getStorage('node')->create([
       'type' => 'oe_news',
       'title' => 'Test news node',
+      'oe_news_types' => 'http://publications.europa.eu/resource/authority/resource-type/ARTICLE_NEWS',
       'oe_teaser' => 'News teaser',
       'oe_summary' => 'News summary',
-      'body' => 'Body',
-      'oe_news_types' => [
-        'http://publications.europa.eu/resource/authority/resource-type/FACTSHEET',
-        'http://publications.europa.eu/resource/authority/resource-type/PUB_GEN',
-      ],
+      'body' => 'News body',
       'oe_reference_code' => 'News reference',
       'oe_publication_date' => [
         'value' => '2020-09-18',
       ],
-      'oe_author' => [
-        'http://publications.europa.eu/resource/authority/corporate-body/ACJHR',
-        'http://publications.europa.eu/resource/authority/corporate-body/ACM',
-      ],
-      'oe_news_location' => 'http://publications.europa.eu/resource/authority/place/ARE_AUH',
+      'oe_author' => 'http://publications.europa.eu/resource/authority/corporate-body/ACJHR',
       'oe_content_content_owner' => 'http://publications.europa.eu/resource/authority/corporate-body/COMMU',
       'uid' => 0,
       'status' => 1,
@@ -70,21 +63,21 @@ class ContentNewsRenderTest extends ContentRenderTestBase {
     $node->save();
     $this->drupalGet($node->toUrl());
 
-    // Assert news contacts.
-    $this->assertContactEntityDefaultDisplay($node, 'oe_news_contacts');
-
-    // Assert news contacts header.
-    $contacts = $this->assertSession()->elementExists('css', 'div#news-contacts');
-    $this->assertContentHeader($contacts, 'Contacts');
-
     // Assert page header - metadata.
     $page_header = $this->assertSession()->elementExists('css', '.ecl-page-header-core');
     $assert = new PatternPageHeaderAssert();
     $expected_values = [
       'title' => 'Test news node',
       'description' => 'News summary',
-      'meta' => 'Factsheet, General publications | 18 September 2020 | Abu Dhabi | African Court of Justice and Human Rights, Arab Common Market',
+      'meta' => 'News article | 18 September 2020 | African Court of Justice and Human Rights',
     ];
+    $assert->assertPattern($expected_values, $page_header->getOuterHtml());
+
+    $node->set('oe_news_location', 'http://publications.europa.eu/resource/authority/place/ARE_AUH');
+    $node->set('oe_news_types', 'http://publications.europa.eu/resource/authority/resource-type/PUB_GEN');
+    $node->save();
+    $this->drupalGet($node->toUrl());
+    $expected_values['meta'] = 'General publications | 18 September 2020 | Abu Dhabi | African Court of Justice and Human Rights';
     $assert->assertPattern($expected_values, $page_header->getOuterHtml());
 
     // Assert news details.
@@ -101,8 +94,8 @@ class ContentNewsRenderTest extends ContentRenderTestBase {
           'body' => '18 September 2020',
         ],
         [
-          'label' => 'Authors',
-          'body' => 'African Court of Justice and Human Rights | Arab Common Market',
+          'label' => 'Author',
+          'body' => 'African Court of Justice and Human Rights',
         ],
         [
           'label' => 'Location',
@@ -125,6 +118,30 @@ class ContentNewsRenderTest extends ContentRenderTestBase {
     $details_expected_values['items'][2]['body'] = 'African Court of Justice and Human Rights | Centre for the Development of Enterprise';
     $details_html = $details->getHtml();
     $field_list_assert->assertPattern($details_expected_values, $details_html);
+
+    // Assert Body field.
+    $body = $this->assertSession()->elementExists('css', 'article[role=article] .ecl-editor');
+    $this->assertEquals('News body', $body->getText());
+
+    // Assert news contacts.
+    $this->assertContactEntityDefaultDisplay($node, 'oe_news_contacts');
+
+    // Assert news contacts header.
+    $contacts = $this->assertSession()->elementExists('css', 'div#news-contacts');
+    $this->assertContentHeader($contacts, 'Contacts');
+
+    // Assert Feaured media field.
+    $this->assertSession()->elementNotExists('css', 'article[role=article] article.ecl-u-type-paragraph.ecl-u-mb-l');
+
+    $media = $this->createMediaImage('news_featured_media');
+    $node->set('oe_news_featured_media', ['target_id' => (int) $media->id()]);
+    $node->save();
+    $this->drupalGet($node->toUrl());
+
+    $picture = $this->assertSession()->elementExists('css', 'article[role=article] article.ecl-u-type-paragraph.ecl-u-mb-l picture');
+    $image = $this->assertSession()->elementExists('css', 'img.ecl-u-width-100.ecl-u-height-auto', $picture);
+    $this->assertContains('placeholder_news_featured_media.png', $image->getAttribute('src'));
+    $this->assertEquals('Alternative text news_featured_media', $image->getAttribute('alt'));
   }
 
 }
