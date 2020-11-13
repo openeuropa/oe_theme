0e6d82e6fe5e4f287237bf8c8d25cebe6d71a069
drishu <aszilagyi@live.com>
2020-11-13 15:41:53 +0000

    EWPP-435: Update teaser config and template.

--- modules/oe_theme_content_publication/oe_theme_content_publication.post_update.php
+++ modules/oe_theme_content_publication/oe_theme_content_publication.post_update.php
@@ -128,3 +128,23 @@ function oe_theme_content_publication_post_update_00007() {
   $entity = $entity_storage->createFromStorageRecord($config);
   $entity->save();
 }
+
+/**
+ * Override publication teaser view display.
+ */
+function oe_theme_content_publication_post_update_00007(): void {
+  $storage = new FileStorage(drupal_get_path('module', 'oe_theme_content_publication') . '/config/post_updates/00007_override_teaser_view_display');
+  $display_values = $storage->read('core.entity_view_display.node.oe_publication.teaser');
+  $storage = \Drupal::entityTypeManager()->getStorage('entity_view_display');
+
+  // Take over teaser view display, regardless if it already exists or not.
+  $view_display = EntityViewDisplay::load($display_values['id']);
+  if ($view_display) {
+    $display = $storage->updateFromStorageRecord($view_display, $display_values);
+    $display->save();
+    return;
+  }
+
+  $display = $storage->createFromStorageRecord($display_values);
+  $display->save();
+}
