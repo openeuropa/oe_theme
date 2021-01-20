47701f0320b75c745b3cb4458f49325ebd0415dd
nagyad <joevagyok@gmail.com>
2021-01-20 17:26:40 +0100

    EWPP-614: Remove utility class and reuse the multi value twig template.

--- modules/oe_theme_content_news/src/Plugin/PageHeaderMetadata/NewsContentType.php
+++ modules/oe_theme_content_news/src/Plugin/PageHeaderMetadata/NewsContentType.php
@@ -7,8 +7,8 @@ namespace Drupal\oe_theme_content_news\Plugin\PageHeaderMetadata;
 use Drupal\Core\Datetime\DateFormatterInterface;
 use Drupal\Core\Entity\EntityRepositoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Field\EntityReferenceFieldItemListInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
-use Drupal\oe_theme_helper\ContentUtility;
 use Drupal\oe_theme_helper\Plugin\PageHeaderMetadata\NodeViewRoutesBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
@@ -107,7 +107,7 @@ class NewsContentType extends NodeViewRoutesBase {
 
     // Add news types to page metadata.
     if (!$node->get('oe_news_types')->isEmpty()) {
-      $metadata['metas'][] = ContentUtility::getCommaSeparatedReferencedEntityLabels($node->get('oe_news_types'));
+      $metadata['metas'][] = $this->getCommaSeparatedReferencedEntityLabels($node->get('oe_news_types'));
     }
 
     // Add publication date to page metadata.
@@ -116,15 +116,33 @@ class NewsContentType extends NodeViewRoutesBase {
 
     // Add news locations to page metadata.
     if (!$node->get('oe_news_location')->isEmpty()) {
-      $metadata['metas'][] = ContentUtility::getCommaSeparatedReferencedEntityLabels($node->get('oe_news_location'));
+      $metadata['metas'][] = $this->getCommaSeparatedReferencedEntityLabels($node->get('oe_news_location'));
     }
 
     // Add news authors to page metadata.
     if (!$node->get('oe_author')->isEmpty()) {
-      $metadata['metas'][] = ContentUtility::getCommaSeparatedReferencedEntityLabels($node->get('oe_author'));
+      $metadata['metas'][] = $this->getCommaSeparatedReferencedEntityLabels($node->get('oe_author'));
     }
 
     return $metadata;
+  }
+
+  /**
+   * Format a list of entity references into a comma separated string.
+   *
+   * @param \Drupal\Core\Field\EntityReferenceFieldItemListInterface $items
+   *   Field item list object.
+   *
+   * @return string
+   *   Comma separated string.
+   */
+  protected function getCommaSeparatedReferencedEntityLabels(EntityReferenceFieldItemListInterface $items): string {
+    $list = [];
+    $entities = $items->referencedEntities();
+    foreach ($entities as $entity) {
+      $list[] = $this->entityRepository->getTranslationFromContext($entity)->label();
+    }
+    return implode(', ', $list);
   }
 
 }
--- modules/oe_theme_content_publication/src/Plugin/PageHeaderMetadata/PublicationContentType.php
+++ modules/oe_theme_content_publication/src/Plugin/PageHeaderMetadata/PublicationContentType.php
@@ -4,9 +4,12 @@ declare(strict_types = 1);
 
 namespace Drupal\oe_theme_content_publication\Plugin\PageHeaderMetadata;
 
+use Drupal\Core\Entity\EntityRepositoryInterface;
+use Drupal\Core\Field\EntityReferenceFieldItemListInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
-use Drupal\oe_theme_helper\ContentUtility;
 use Drupal\oe_theme_helper\Plugin\PageHeaderMetadata\NodeViewRoutesBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Page header metadata for the OpenEuropa Publication content entity.
@@ -20,6 +23,45 @@ use Drupal\oe_theme_helper\Plugin\PageHeaderMetadata\NodeViewRoutesBase;
 class PublicationContentType extends NodeViewRoutesBase {
 
   use StringTranslationTrait;
+
+  /**
+   * The entity repository.
+   *
+   * @var \Drupal\Core\Entity\EntityRepositoryInterface
+   */
+  protected $entityRepository;
+
+  /**
+   * Creates a new PublicationContentType object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $event_dispatcher
+   *   The event dispatcher.
+   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
+   *   The entity repository service.
+   */
+  public function __construct(array $configuration, string $plugin_id, $plugin_definition, EventDispatcherInterface $event_dispatcher, EntityRepositoryInterface $entity_repository) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $event_dispatcher);
+    $this->entityRepository = $entity_repository;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('event_dispatcher'),
+      $container->get('entity.repository')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -37,9 +79,27 @@ class PublicationContentType extends NodeViewRoutesBase {
     $node = $this->getNode();
 
     $metadata = parent::getMetadata();
-    $metadata['metas'][] = ContentUtility::getCommaSeparatedReferencedEntityLabels($node->get('oe_publication_type'));
+    $metadata['metas'][] = $this->getCommaSeparatedReferencedEntityLabels($node->get('oe_publication_type'));
 
     return $metadata;
+  }
+
+  /**
+   * Format a list of entity references into a comma separated string.
+   *
+   * @param \Drupal\Core\Field\EntityReferenceFieldItemListInterface $items
+   *   Field item list object.
+   *
+   * @return string
+   *   Comma separated string.
+   */
+  protected function getCommaSeparatedReferencedEntityLabels(EntityReferenceFieldItemListInterface $items): string {
+    $list = [];
+    $entities = $items->referencedEntities();
+    foreach ($entities as $entity) {
+      $list[] = $this->entityRepository->getTranslationFromContext($entity)->label();
+    }
+    return implode(', ', $list);
   }
 
 }
