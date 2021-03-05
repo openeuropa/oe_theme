a213900a57795434bcfa12bb352da3717644581f
Evgenii Nikitin <yenyasinn@gmail.com>
2021-03-05 13:19:47 +0300

    EWPP-696: Full Teaser test.

--- tests/Kernel/PersonRenderTest.php
+++ tests/Kernel/PersonRenderTest.php
@@ -4,12 +4,17 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\oe_theme\Kernel;
 
-use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\node\Entity\Node;
+use Drupal\node\NodeInterface;
+use Drupal\oe_content_entity_contact\Entity\Contact;
+use Drupal\oe_content_entity_contact\Entity\ContactInterface;
+use Drupal\oe_content_person\Entity\PersonJob;
+use Drupal\oe_content_person\Entity\PersonJobInterface;
 use Drupal\Tests\oe_theme\PatternAssertions\ListItemAssert;
 use Drupal\Tests\oe_theme\PatternAssertions\FieldListAssert;
 use Drupal\Tests\oe_theme\PatternAssertions\PatternAssertState;
 use Drupal\Tests\user\Traits\UserCreationTrait;
+use Symfony\Component\DomCrawler\Crawler;
 
 /**
  * Tests consultation rendering.
@@ -62,6 +67,8 @@ class PersonRenderTest extends ContentRenderTestBase {
 
     module_load_include('install', 'oe_content');
     oe_content_install();
+    module_load_include('install', 'oe_content_person');
+    oe_content_person_install();
 
     $this->setUpCurrentUser([], [], TRUE);
   }
@@ -142,12 +149,191 @@ class PersonRenderTest extends ContentRenderTestBase {
       ]),
     ];
     $assert->assertPattern($expected_values, $this->getRenderedNode($node));
+
+    // Assert Contacts field.
+    $general_contact = $this->createContactEntity('direct_contact', 'oe_general');
+    $node->set('oe_person_contacts', $general_contact)->save();
+    $expected_values['additional_information'][] = new PatternAssertState(new FieldListAssert(), [
+      'items' => [
+        [
+          'label' => 'Email',
+          'body' => 'direct_contact@example.com',
+        ], [
+          'label' => 'Phone number',
+          'body' => 'Phone number direct_contact',
+        ], [
+          'label' => 'Mobile number',
+          'body' => 'Mobile number direct_contact',
+        ], [
+          'label' => 'Fax number',
+          'body' => 'Fax number direct_contact',
+        ], [
+          'label' => 'Address',
+          'body' => 'Address direct_contact1001BrusselsBelgium',
+        ], [
+          'label' => 'Office',
+          'body' => 'Office direct_contact',
+        ], [
+          'label' => 'Social media links',
+          'body' => ' Social media direct_contact',
+        ],
+      ],
+    ]);
+    $assert->assertPattern($expected_values, $this->getRenderedNode($node));
+
+    // Assert Contacts field with an organisation as contact.
+    $organisation_reference_contact = $this->createContactOrganisationReferenceEntity('organisation_reference');
+    $node->set('oe_person_contacts', [
+      $general_contact,
+      $organisation_reference_contact,
+    ])->save();
+
+    $html = $this->getRenderedNode($node);
+    $crawler = new Crawler($html);
+    $first_contact_render = $crawler->filter('article .ecl-content-item__additional_information:nth-child(3) div.ecl-u-border-bottom.ecl-u-border-color-grey-15.ecl-u-mb-m.ecl-u-pb-m');
+    $this->assertCount(1, $first_contact_render);
+
+    $field_assert = new FieldListAssert();
+    $second_contact_expected_values = [
+      'items' => [
+        [
+          'label' => 'Email',
+          'body' => 'organisation_reference_contact@example.com',
+        ], [
+          'label' => 'Phone number',
+          'body' => 'Phone number organisation_reference_contact',
+        ], [
+          'label' => 'Mobile number',
+          'body' => 'Mobile number organisation_reference_contact',
+        ], [
+          'label' => 'Fax number',
+          'body' => 'Fax number organisation_reference_contact',
+        ], [
+          'label' => 'Address',
+          'body' => 'Address organisation_reference_contact1001BrusselsBelgium',
+        ], [
+          'label' => 'Office',
+          'body' => 'Office organisation_reference_contact',
+        ], [
+          'label' => 'Social media links',
+          'body' => ' Social media organisation_reference_contact',
+        ],
+      ],
+    ];
+    $second_contact_render = $crawler->filter('article .ecl-content-item__additional_information:nth-child(3) > div:nth-child(2)');
+    $field_assert->assertPattern($second_contact_expected_values, $second_contact_render->html());
+
+    // Assert Jobs field.
+    $job_1 = $this->createPersonJobEntity('job_1', [
+      'oe_acting' => TRUE,
+      'oe_role_reference' => 'http://publications.europa.eu/resource/authority/role/MEMBER',
+    ]);
+    $node->set('oe_person_contacts', NULL);
+    $node->set('oe_person_jobs', $job_1)->save();
+    $expected_values['meta'] = '(Acting) Member';
+    $expected_values['additional_information'][1] = new PatternAssertState(new FieldListAssert(), [
+      'items' => [
+        [
+          'label' => '(Acting) Member',
+          'body' => 'Description job_1',
+        ],
+      ],
+    ]);
+    $assert->assertPattern($expected_values, $this->getRenderedNode($node));
+
+    $job_2 = $this->createPersonJobEntity('job_2', ['oe_role_reference' => 'http://publications.europa.eu/resource/authority/role/ADVOC']);
+    $node->set('oe_person_jobs', [$job_1, $job_2])->save();
+    $expected_values['meta'] = '(Acting) Member | Advocate';
+    $expected_values['additional_information'][1] = new PatternAssertState(new FieldListAssert(), [
+      'items' => [
+        [
+          'label' => '(Acting) Member',
+          'body' => 'Description job_1',
+        ], [
+          'label' => 'Advocate',
+          'body' => 'Description job_2',
+        ],
+      ],
+    ]);
+    $assert->assertPattern($expected_values, $this->getRenderedNode($node));
+
+    // Assert non-eu person.
+    $job_1->set('oe_role_name', 'Singer');
+    $job_1->set('oe_role_reference', NULL);
+    $job_1->set('oe_acting', NULL)->save();
+    $job_2->set('oe_role_reference', NULL);
+    $job_2->set('oe_role_name', 'Dancer')->save();
+    $node->set('oe_person_type', 'non_eu');
+    $node->set('oe_person_contacts', $general_contact);
+    $organisation_node = $this->createOrganisationNode('non_eu');
+    $node->set('oe_person_organisation', $organisation_node)->save();
+
+    $expected_values = [
+      'title' => 'Jagger Mick',
+      'meta' => 'Singer | Dancer',
+      'image' => [
+        'src' => 'person_portrait.png',
+        'alt' => '',
+      ],
+      'additional_information' => [
+        new PatternAssertState(new FieldListAssert(), [
+          'items' => [
+            [
+              'label' => 'Organisation',
+              'body' => 'Organisation node non_eu',
+            ],
+          ],
+        ]),
+        new PatternAssertState(new FieldListAssert(), [
+          'items' => [
+            [
+              'label' => 'Email',
+              'body' => 'direct_contact@example.com',
+            ], [
+              'label' => 'Phone number',
+              'body' => 'Phone number direct_contact',
+            ], [
+              'label' => 'Mobile number',
+              'body' => 'Mobile number direct_contact',
+            ], [
+              'label' => 'Fax number',
+              'body' => 'Fax number direct_contact',
+            ], [
+              'label' => 'Address',
+              'body' => 'Address direct_contact1001BrusselsBelgium',
+            ], [
+              'label' => 'Office',
+              'body' => 'Office direct_contact',
+            ], [
+              'label' => 'Social media links',
+              'body' => ' Social media direct_contact',
+            ],
+          ],
+        ]),
+        new PatternAssertState(new FieldListAssert(), [
+          'items' => [
+            [
+              'label' => 'Singer',
+              'body' => 'Description job_1',
+            ], [
+              'label' => 'Dancer',
+              'body' => 'Description job_2',
+            ],
+          ],
+        ]),
+      ],
+    ];
+    $html = $this->getRenderedNode($node);
+    $assert->assertPattern($expected_values, $html);
+    $crawler = new Crawler($html);
+    $jobs_render = $crawler->filter('article .ecl-content-item__additional_information:nth-child(5) div.ecl-u-border-top.ecl-u-border-color-grey-15.ecl-u-pt-m');
+    $this->assertCount(1, $jobs_render);
   }
 
   /**
    * Renders node using provided view mode.
    *
-   * @param \Drupal\Core\Entity\ContentEntityInterface $node
+   * @param \Drupal\node\NodeInterface $node
    *   Node entity.
    * @param string $view_mode
    *   Node view mode.
@@ -155,9 +341,80 @@ class PersonRenderTest extends ContentRenderTestBase {
    * @return string
    *   Rendered content.
    */
-  protected function getRenderedNode(ContentEntityInterface $node, $view_mode = 'teaser'): string {
+  protected function getRenderedNode(NodeInterface $node, $view_mode = 'teaser'): string {
     $build = $this->nodeViewBuilder->view($node, $view_mode);
     return $this->renderRoot($build);
+  }
+
+  /**
+   * Creates Contact entity Organisation reference bundle.
+   *
+   * @param string $name
+   *   Name of the entity.
+   *
+   * @return \Drupal\oe_content_entity_contact\Entity\ContactInterface
+   *   Contact entity.
+   */
+  protected function createContactOrganisationReferenceEntity(string $name): ContactInterface {
+    $organisation_node = $this->createOrganisationNode($name);
+
+    $contact = Contact::create([
+      'bundle' => 'oe_organisation_reference',
+      'name' => $name,
+      'oe_node_reference' => $organisation_node,
+      'status' => 1,
+    ]);
+    $contact->save();
+
+    return $contact;
+  }
+
+  /**
+   * Creates Organisation node.
+   *
+   * @param string $name
+   *   Name of the entity.
+   *
+   * @return \Drupal\node\NodeInterface
+   *   Node entity.
+   */
+  protected function createOrganisationNode(string $name): NodeInterface {
+    $contact = $this->createContactEntity($name . '_contact', 'oe_general');
+
+    $node = Node::create([
+      'type' => 'oe_organisation',
+      'title' => "Organisation node $name",
+      'oe_content_owner' => 'http://publications.europa.eu/resource/authority/corporate-body/ABEC',
+      'oe_organisation_org_type' => 'eu',
+      'oe_organisation_eu_org' => 'http://publications.europa.eu/resource/authority/corporate-body/ACM',
+      'oe_organisation_contact' => $contact,
+      'status' => 1,
+    ]);
+    $node->save();
+
+    return $node;
+  }
+
+  /**
+   * Creates Person job entity.
+   *
+   * @param string $name
+   *   String to be used in test data.
+   * @param array $values
+   *   Entity values.
+   *
+   * @return \Drupal\oe_content_person\Entity\PersonJobInterface
+   *   Person job entity
+   */
+  protected function createPersonJobEntity(string $name, array $values): PersonJobInterface {
+    $values = [
+      'type' => 'default',
+      'oe_description' => "Description $name",
+    ] + $values;
+    $person_job = PersonJob::create($values);
+    $person_job->save();
+
+    return $person_job;
   }
 
 }
