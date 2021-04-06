dacdbe7337c5896bc4d28e30e23721c91f3c56b0
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2021-04-06 10:16:53 +0300

    EWPP-856: Implementation of block plugin and js.

---
+++ modules/oe_theme_inpage_navigation/src/Plugin/Block/InpageNavigationBlock.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_theme_inpage_navigation\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides an In-page navigation block.
+ *
+ * @Block(
+ *   id = "oe_theme_inpage_navigation_menu",
+ *   admin_label = @Translation("Inpage Navigation block"),
+ *   category = @Translation("OpenEuropa"),
+ * )
+ */
+class InpageNavigationBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build['content'] = [
+      '#theme' => 'oe_theme_inpage_navigation_block',
+      '#title' => $this->t('Page contents'),
+    ];
+    return $build;
+  }
+
+}
