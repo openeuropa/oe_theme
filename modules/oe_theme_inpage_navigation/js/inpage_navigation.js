7363749a1da490056b468d88aa85a32a2d921e96
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2021-04-07 19:59:16 +0300

    EWPP-856: Update javascript file and template for block.

--- modules/oe_theme_inpage_navigation/js/inpage_navigation.js
+++ modules/oe_theme_inpage_navigation/js/inpage_navigation.js
@@ -9,14 +9,22 @@
       var headers = document.querySelectorAll('h2.ecl-u-type-heading-2');
       li_html = [];
       for (var h = 0; h < headers.length; h++) {
+        if (!headers[h].hasAttribute('id')) {
+          continue;
+        }
         id = headers[h].getAttribute('id');
         title = headers[h].innerHTML;
         li_html.push('<li class="ecl-inpage-navigation__item"><a href="#' + id + '" class="ecl-link ecl-link--standalone ecl-inpage-navigation__link" data-ecl-inpage-navigation-link="">' + title + '</a></li>');
       }
+
+      if (li_html.length === 0) {
+        document.querySelector('.oe-theme-left-sidebar').remove();
+        document.querySelector('.oe-theme-content-region').setAttribute('class', 'ecl-col-12');
+        return;
+      }
+
       for (var i = 0; i < elements.length; i++) {
         elements[i].querySelector('ul').innerHTML = li_html.join(' ');
-        var inpage_navigation = new ECL.InpageNavigation(elements[i]);
-        inpage_navigation.init();
       }
     }
   };
