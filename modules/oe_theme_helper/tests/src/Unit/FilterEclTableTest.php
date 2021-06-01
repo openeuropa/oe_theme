da885cfcef9dd112dec2ac95961863be4e9f6a59
Francesco Sardara <francesco.sardara@ausy.be>
2021-06-01 08:25:57 +0200

    EWPP-1087: Add more test scenarios to cover all the code.

--- modules/oe_theme_helper/tests/src/Unit/FilterEclTableTest.php
+++ modules/oe_theme_helper/tests/src/Unit/FilterEclTableTest.php
@@ -43,15 +43,11 @@ class FilterEclTableTest extends UnitTestCase {
    */
   public function processDataProvider(): array {
     return [
-      'Simple table with header - class and data-ecl-table-header are added' => [
-        '<p>Some text before table</p><table><caption>Caption</caption><thead><tr><th>Column 1</th><th>Column 2</th></tr></thead><tbody><tr><td colspan="1">1-1</td><td>1-2</td></tr><tr><td rowspan="1">2-1</td><td>2-2</td></tr></tbody></table><p>Some text after table</p>',
-        '<p>Some text before table</p><table><caption>Caption</caption><thead><tr><th>Column 1</th><th>Column 2</th></tr></thead><tbody><tr><td colspan="1" class="ecl-table__cell" data-ecl-table-header="Column 1">1-1</td><td class="ecl-table__cell" data-ecl-table-header="Column 2">1-2</td></tr><tr><td rowspan="1" class="ecl-table__cell" data-ecl-table-header="Column 1">2-1</td><td class="ecl-table__cell" data-ecl-table-header="Column 2">2-2</td></tr></tbody></table><p>Some text after table</p>',
+      'Full table with thead and tfoot' => [
+        '<p>Some text before table</p><table><caption>Caption</caption><thead><tr><th>Column 1</th><th>Column 2</th></tr></thead><tbody><tr><td colspan="1">1-1</td><td>1-2</td></tr><tr><td rowspan="1">2-1</td><td>2-2</td></tr></tbody><tfoot><tr><td>Footer 1</td><td>Footer 2</td></tr></tfoot></table><p>Some text after table</p>',
+        '<p>Some text before table</p><table><caption>Caption</caption><thead><tr><th>Column 1</th><th>Column 2</th></tr></thead><tbody><tr><td colspan="1" class="ecl-table__cell" data-ecl-table-header="Column 1">1-1</td><td class="ecl-table__cell" data-ecl-table-header="Column 2">1-2</td></tr><tr><td rowspan="1" class="ecl-table__cell" data-ecl-table-header="Column 1">2-1</td><td class="ecl-table__cell" data-ecl-table-header="Column 2">2-2</td></tr></tbody><tfoot><tr><td class="ecl-table__cell" data-ecl-table-header="Column 1">Footer 1</td><td class="ecl-table__cell" data-ecl-table-header="Column 2">Footer 2</td></tr></tfoot></table><p>Some text after table</p>',
       ],
-      'Table with footer - class and data-ecl-table-header are added' => [
-        '<div><table><caption>Caption</caption><thead><tr><th>Column 1</th><th>Column 2</th></tr></thead><tbody><tr><td colspan="1">1-1</td><td>1-2</td></tr><tr><td rowspan="1">2-1</td><td>2-2</td></tr></tbody><tfoot><tr><td>Footer 1</td><td>Footer 2</td></tr></tfoot></table></div>',
-        '<div><table><caption>Caption</caption><thead><tr><th>Column 1</th><th>Column 2</th></tr></thead><tbody><tr><td colspan="1" class="ecl-table__cell" data-ecl-table-header="Column 1">1-1</td><td class="ecl-table__cell" data-ecl-table-header="Column 2">1-2</td></tr><tr><td rowspan="1" class="ecl-table__cell" data-ecl-table-header="Column 1">2-1</td><td class="ecl-table__cell" data-ecl-table-header="Column 2">2-2</td></tr></tbody><tfoot><tr><td class="ecl-table__cell" data-ecl-table-header="Column 1">Footer 1</td><td class="ecl-table__cell" data-ecl-table-header="Column 2">Footer 2</td></tr></tfoot></table></div>',
-      ],
-      'Table with vertical header - class is added' => [
+      'Table with vertical header - only class is added' => [
         '<table><tbody><tr><th>Row 1</th><td>1-1</td><td>1-2</td></tr><tr><th>Row 2</th><td>2-1</td><td>2-2</td></tr><tr><th>Row 3</th><td>3-1</td><td>3-2</td></tr></tbody></table>',
         '<table><tbody><tr><th class="ecl-table__cell">Row 1</th><td class="ecl-table__cell">1-1</td><td class="ecl-table__cell">1-2</td></tr><tr><th class="ecl-table__cell">Row 2</th><td class="ecl-table__cell">2-1</td><td class="ecl-table__cell">2-2</td></tr><tr><th class="ecl-table__cell">Row 3</th><td class="ecl-table__cell">3-1</td><td class="ecl-table__cell">3-2</td></tr></tbody></table>',
       ],
@@ -63,21 +59,49 @@ class FilterEclTableTest extends UnitTestCase {
         '<table><caption>Caption</caption><thead><tr><th>Column 1</th><th>Column 2</th></tr></thead><tr><td>1-1</td><td>1-2</td></tr><tr><td>2-1</td><td>2-2</td></tr></table>',
         '<table><caption>Caption</caption><thead><tr><th>Column 1</th><th>Column 2</th></tr></thead><tr><td class="ecl-table__cell" data-ecl-table-header="Column 1">1-1</td><td class="ecl-table__cell" data-ecl-table-header="Column 2">1-2</td></tr><tr><td class="ecl-table__cell" data-ecl-table-header="Column 1">2-1</td><td class="ecl-table__cell" data-ecl-table-header="Column 2">2-2</td></tr></table>',
       ],
-      'Simple table without headers - leave as-is' => [
+      'Multiple tables' => [
+        '<table><thead><tr><th>Column A1</th><th>Column A2</th></tr></thead><tbody><tr><td>A1-1</td><td>A1-2</td></tr></tbody><tfoot><tr><td>A2-1</td><td>A2-2</td></tr></tfoot></table><table><tr><td>B1-1</td><td>B1-2</td></tr><tr><td>B2-1</td><td>B2-2</td></tr></table><table><thead><tr><th>Column C1</th><th>Column C2</th><th>Column C3</th></tr></thead><tr><td>C1-1</td><td>C1-2</td><td>C1-3</td></tr><tr><td>C2-1</td><td>C2-2</td><td>C2-3</td></tr></table>',
+        '<table><thead><tr><th>Column A1</th><th>Column A2</th></tr></thead><tbody><tr><td class="ecl-table__cell" data-ecl-table-header="Column A1">A1-1</td><td class="ecl-table__cell" data-ecl-table-header="Column A2">A1-2</td></tr></tbody><tfoot><tr><td class="ecl-table__cell" data-ecl-table-header="Column A1">A2-1</td><td class="ecl-table__cell" data-ecl-table-header="Column A2">A2-2</td></tr></tfoot></table><table><tr><td>B1-1</td><td>B1-2</td></tr><tr><td>B2-1</td><td>B2-2</td></tr></table><table><thead><tr><th>Column C1</th><th>Column C2</th><th>Column C3</th></tr></thead><tr><td class="ecl-table__cell" data-ecl-table-header="Column C1">C1-1</td><td class="ecl-table__cell" data-ecl-table-header="Column C2">C1-2</td><td class="ecl-table__cell" data-ecl-table-header="Column C3">C1-3</td></tr><tr><td class="ecl-table__cell" data-ecl-table-header="Column C1">C2-1</td><td class="ecl-table__cell" data-ecl-table-header="Column C2">C2-2</td><td class="ecl-table__cell" data-ecl-table-header="Column C3">C2-3</td></tr></table>',
+      ],
+      'Table without any th - not processed' => [
         '<table><tbody><tr><td>1-1</td><td>1-2</td></tr><tr><td>2-1</td><td>2-2</td></tr><tr><td>3-1</td><td>3-2</td></tr></tbody></table>',
         '<table><tbody><tr><td>1-1</td><td>1-2</td></tr><tr><td>2-1</td><td>2-2</td></tr><tr><td>3-1</td><td>3-2</td></tr></tbody></table>',
       ],
-      'Table contains rowspan - leave as-is' => [
+      'Table with cells spanning multiple rows - not processed' => [
         '<table><thead><tr><th>Column 1</th><th>Column 2</th><th>Column 3</th></tr></thead><tbody><tr><td rowspan="2">1-1</td><td>1-2</td><td>1-3</td></tr><tr><td>2-2</td><td>2-3</td></tr></tbody></table>',
         '<table><thead><tr><th>Column 1</th><th>Column 2</th><th>Column 3</th></tr></thead><tbody><tr><td rowspan="2">1-1</td><td>1-2</td><td>1-3</td></tr><tr><td>2-2</td><td>2-3</td></tr></tbody></table>',
       ],
-      'Table contains colspan - leave as-is' => [
+      'Table with cells spanning multiple columns - not processed' => [
+        '<table><thead><tr><th colspan="2">Column 1</th><th>Column 3</th></tr></thead><tbody><tr><td>1-1</td><td>1-2</td><td>1-</td></tr><tr><td>2-1</td><td>2-2</td><td>2-3</td></tr></tbody></table>',
+        '<table><thead><tr><th colspan="2">Column 1</th><th>Column 3</th></tr></thead><tbody><tr><td>1-1</td><td>1-2</td><td>1-</td></tr><tr><td>2-1</td><td>2-2</td><td>2-3</td></tr></tbody></table>',
+      ],
+      'Table with header cells spanning multiple rows - not processed' => [
+        '<table><thead><tr><th rowspan="2">Column 1</th><th>Column 2</th></tr><tr><th>Column 4</th></tr></thead><tbody><tr><td>1-1</td><td>1-2</td></tr><tr><td>2-1</td><td>2-2</td></tr></tbody></table>',
+        '<table><thead><tr><th rowspan="2">Column 1</th><th>Column 2</th></tr><tr><th>Column 4</th></tr></thead><tbody><tr><td>1-1</td><td>1-2</td></tr><tr><td>2-1</td><td>2-2</td></tr></tbody></table>',
+      ],
+      'Table with header cells spanning multiple columns - not processed' => [
         '<table><thead><tr><th>Column 1</th><th>Column 2</th><th>Column 3</th></tr></thead><tbody><tr><td colspan="2">1-1</td><td>1-3</td></tr><tr><td>2-1</td><td>2-2</td><td>2-3</td></tr></tbody></table>',
         '<table><thead><tr><th>Column 1</th><th>Column 2</th><th>Column 3</th></tr></thead><tbody><tr><td colspan="2">1-1</td><td>1-3</td></tr><tr><td>2-1</td><td>2-2</td><td>2-3</td></tr></tbody></table>',
       ],
-      'Table with multi header - leave as-is' => [
-        '<table><thead><tr><th>Column 1</th><th colspan="2">Columns 2 - 3</th><th>Column 4</th></tr><tr><th></th><th>Column 2</th><th>Column 3</th><th></th></tr></thead><tbody><tr><td>1-1</td><td>1-2</td><td>1-3</td><td>1-4</td></tr><tr><td>2-1</td><td>2-2</td><td>2-3</td><td>2-4</td></tr></tbody></table>',
-        '<table><thead><tr><th>Column 1</th><th colspan="2">Columns 2 - 3</th><th>Column 4</th></tr><tr><th></th><th>Column 2</th><th>Column 3</th><th></th></tr></thead><tbody><tr><td>1-1</td><td>1-2</td><td>1-3</td><td>1-4</td></tr><tr><td>2-1</td><td>2-2</td><td>2-3</td><td>2-4</td></tr></tbody></table>',
+      'Table with th cell in body - not processed' => [
+        '<table><thead><tr><th>Column 1</th><th>Column 2</th></tr></thead><tr><td>1-1</td><th>1-2</th></tr><tr><td>2-1</td><td>2-2</td></tr></table>',
+        '<table><thead><tr><th>Column 1</th><th>Column 2</th></tr></thead><tr><td>1-1</td><th>1-2</th></tr><tr><td>2-1</td><td>2-2</td></tr></table>',
+      ],
+      'Table with multiple header rows - first row is used' => [
+        '<table><thead><tr><th>Column 1</th><th>Column 2</th><th>Column 3</th></tr><tr><th>Column 4</th><th>Column 5</th><th>Column 6</th></tr></thead><tbody><tr><td>1-1</td><td>1-2</td><td>1-3</td></tr></tbody></table>',
+        '<table><thead><tr><th>Column 1</th><th>Column 2</th><th>Column 3</th></tr><tr><th>Column 4</th><th>Column 5</th><th>Column 6</th></tr></thead><tbody><tr><td class="ecl-table__cell" data-ecl-table-header="Column 1">1-1</td><td class="ecl-table__cell" data-ecl-table-header="Column 2">1-2</td><td class="ecl-table__cell" data-ecl-table-header="Column 3">1-3</td></tr></tbody></table>',
+      ],
+      'Table header with invalid header - only class is added' => [
+        '<table><thead><tr><th>Column 1</th><td>Column 2</td></tr></thead><tbody><tr><td>1-1</td><td>1-2</td></tr></tbody></table>',
+        '<table><thead><tr><th>Column 1</th><td>Column 2</td></tr></thead><tbody><tr><td class="ecl-table__cell">1-1</td><td class="ecl-table__cell">1-2</td></tr></tbody></table>',
+      ],
+      'Table wrapped in HTML comment - not processed' => [
+        '<!--<table><thead><tr><th>Column 1</th><th>Column 2</th></tr></thead><tbody><tr><td>1-1</td><td>1-2</td></tr><tr><td>2-1</td><td>2-2</td></tr></tbody></table>-->',
+        '<!--<table><thead><tr><th>Column 1</th><th>Column 2</th></tr></thead><tbody><tr><td>1-1</td><td>1-2</td></tr><tr><td>2-1</td><td>2-2</td></tr></tbody></table>-->',
+      ],
+      'No table' => [
+        '<p>Some random text with no tables around.</p>',
+        '<p>Some random text with no tables around.</p>',
       ],
     ];
   }
