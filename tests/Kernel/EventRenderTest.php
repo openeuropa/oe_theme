aff22b702993558778189af3b84a0ec2186b163d
Francesco Sardara <francesco.sardara@ausy.be>
2021-09-08 17:58:02 +0200

    EWPP-1458: Test that events render correctly when a non-existing event type is specified.

--- tests/Kernel/EventRenderTest.php
+++ tests/Kernel/EventRenderTest.php
@@ -259,6 +259,15 @@ class EventRenderTest extends ContentRenderTestBase {
     ]);
     $assert->assertPattern($expected_values, $html);
     $assert->assertVariant('date_cancelled', $html);
+
+    // Verify that the teaser renders correctly when a non-existing event type
+    // is set.
+    $node->set('oe_event_type', 'http://publications.europa.eu/resource/authority/public-event-type/OP_DATPRO')
+      ->save();
+    $build = $this->nodeViewBuilder->view($node, 'teaser', 'bg');
+    $html = $this->renderRoot($build);
+    unset($expected_values['meta']);
+    $assert->assertPattern($expected_values, $html);
   }
 
 }
