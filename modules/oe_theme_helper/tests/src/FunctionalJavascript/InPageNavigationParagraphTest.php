0ea1b00327bf9ff87007ba3792063510c22fba0d
Francesco Sardara <francesco@tdgwebservices.com>
2022-05-09 14:55:30 +0200

    EWPP-2149: Add test coverage for multiple inpage navigation content rows.

--- modules/oe_theme_helper/tests/src/FunctionalJavascript/InPageNavigationParagraphTest.php
+++ modules/oe_theme_helper/tests/src/FunctionalJavascript/InPageNavigationParagraphTest.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\oe_theme_helper\FunctionalJavascript;
 
+use Behat\Mink\Element\NodeElement;
 use Drupal\filter\Entity\FilterFormat;
 use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
 use Drupal\paragraphs\Entity\Paragraph;
@@ -68,7 +69,7 @@ class InPageNavigationParagraphTest extends WebDriverTestBase {
     ]);
     $paragraph->save();
 
-    // Create the main content row paragraph with a default variant.
+    // Create the main content row paragraph with the inpage navigation variant.
     $content_row = Paragraph::create([
       'type' => 'oe_content_row',
       'oe_paragraphs_variant' => 'inpage_navigation',
@@ -97,8 +98,8 @@ class InPageNavigationParagraphTest extends WebDriverTestBase {
     ]);
 
     $this->drupalGet($node->toUrl());
-    $navigation = $this->assertSession()->elementExists('css', '.ecl-col-l-3.ecl-u-z-navigation .ecl-inpage-navigation');
-    $inpage_nav_assert = new InPageNavigationAssert();
+    $containers = $this->getSession()->getPage()->findAll('css', '.inpage-navigation-container');
+    $this->assertCount(1, $containers);
     $inpage_nav_expected_values = [
       'title' => 'Page navigation',
       'list' => [
@@ -106,8 +107,66 @@ class InPageNavigationParagraphTest extends WebDriverTestBase {
         ['label' => 'Here is a heading', 'href' => '#here-is-a-heading'],
       ],
     ];
-    $inpage_nav_assert->assertPattern($inpage_nav_expected_values, $navigation->getOuterHtml());
+    $this->assertInPageNavigation($inpage_nav_expected_values, $containers[0]);
 
+    // Create another content row with a rich text paragraph.
+    $paragraph3 = Paragraph::create([
+      'type' => 'oe_rich_text',
+      'field_oe_title' => 'Second rich text paragraph',
+      'field_oe_text_long' => [
+        'value' => 'Some other text. <h2>Another heading</h2>',
+        'format' => 'full_html',
+      ],
+    ]);
+    $paragraph3->save();
+    $content_row2 = Paragraph::create([
+      'type' => 'oe_content_row',
+      'oe_paragraphs_variant' => 'inpage_navigation',
+      'field_oe_title' => 'Second page navigation',
+      'field_oe_paragraphs' => [$paragraph3],
+    ]);
+    $content_row->save();
+
+    // Reference the two content rows and the extra rich text paragraphs.
+    $node->set('field_oe_demo_body', [
+      $content_row,
+      $paragraph2,
+      $content_row2,
+    ]);
+    $node->save();
+
+    $this->drupalGet($node->toUrl());
+    $containers = $this->getSession()->getPage()->findAll('css', '.inpage-navigation-container');
+    $this->assertCount(2, $containers);
+    // The first inpage navigation didn't change.
+    $this->assertInPageNavigation($inpage_nav_expected_values, $containers[0]);
+    $this->assertInPageNavigation([
+      'title' => 'Second page navigation',
+      'list' => [
+        [
+          'label' => 'Second rich text paragraph',
+          'href' => '#second-rich-text-paragraph',
+        ],
+        [
+          'label' => 'Another heading',
+          'href' => '#another-heading',
+        ],
+      ],
+    ], $containers[1]);
+  }
+
+  /**
+   * Asserts the inpage navigation inside a container.
+   *
+   * @param array $expected
+   *   The expected values.
+   * @param \Behat\Mink\Element\NodeElement $container
+   *   The inpage navigation container.
+   */
+  protected function assertInPageNavigation(array $expected, NodeElement $container): void {
+    $navigation = $this->assertSession()->elementExists('css', '.ecl-col-l-3.ecl-u-z-navigation .ecl-inpage-navigation', $container);
+    $inpage_nav_assert = new InPageNavigationAssert();
+    $inpage_nav_assert->assertPattern($expected, $navigation->getOuterHtml());
   }
 
 }
