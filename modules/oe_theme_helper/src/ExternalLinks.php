b5bc1d8e3fe7e529a2f3e8ebac641f56721a8111
22Alexandra <beldean.ale@gmail.com>
2022-05-09 10:23:02 +0300

    EWPP-2069: Add config file to set internal domain regex.

--- modules/oe_theme_helper/src/ExternalLinks.php
+++ modules/oe_theme_helper/src/ExternalLinks.php
@@ -5,12 +5,30 @@ declare(strict_types = 1);
 namespace Drupal\oe_theme_helper;
 
 use Drupal\Component\Utility\UrlHelper;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Url;
 
 /**
  * Verifies if a URL is considered external or internal.
  */
 class ExternalLinks implements ExternalLinksInterface {
+
+  /**
+   * The configuration factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs an ExternalLinks object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The configuration factory.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
 
   /**
    * {@inheritdoc}
@@ -28,28 +46,12 @@ class ExternalLinks implements ExternalLinksInterface {
       return $external;
     }
 
-    // If it's external link, make sure its domain is not considered internal.
-    $internal_domains = $this->internalDomains();
-    foreach ($internal_domains as $internal_domain) {
-      if (strpos($path, $internal_domain)) {
-        $external = FALSE;
-        break;
-      }
+    // If it's an external link, make sure its domain is not internal.
+    $internal_domain_expression = $this->configFactory->get('oe_theme_helper.internal_domains')->get('internal_domain');
+    if (!$internal_domain_expression) {
+      return $external;
     }
-
-    return $external;
-  }
-
-  /**
-   * Defines a list of domain considered internal.
-   *
-   * @return array|null
-   *   The list of internal domains or NULL if there is none.
-   */
-  protected function internalDomains(): ?array {
-    return [
-      'europa.eu',
-    ];
+    return !preg_match_all($internal_domain_expression, $path);
   }
 
 }
