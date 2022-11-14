27cbad630488412615e0163b8053816138b6c62e
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2022-11-14 16:07:31 +0200

    EWPP-2734: Fix random failure in behat tests related to hovering over a link.

--- modules/oe_theme_helper/tests/src/FunctionalJavascript/WysiwygTableTest.php
+++ modules/oe_theme_helper/tests/src/FunctionalJavascript/WysiwygTableTest.php
@@ -309,7 +309,6 @@ JS;
     $this->getSession()->switchToIFrame($page->find('xpath', "//div[contains(concat(' ', normalize-space(@class), ' '), ' cke_menu_panel ') and not(@hidden)]/iframe")->getAttribute('id'));
     $this->clickLink('Cell');
     $this->getSession()->switchToIFrame();
-    // @todo Fix in EWPP-2734 the way for waiting for the showing of the context submenu.
     $web_assert->waitForElementVisible('xpath', "//div[contains(concat(' ', normalize-space(@class), ' '), ' cke_menu_panel ') and @hidden][last()]/iframe");
     $this->getSession()->switchToIFrame($page->find('xpath', "//div[contains(concat(' ', normalize-space(@class), ' '), ' cke_menu_panel ') and not(@hidden)][last()]/iframe")->getAttribute('id'));
     $this->clickLink('Cell Properties');
