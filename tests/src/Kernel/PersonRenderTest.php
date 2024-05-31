ca671759a5917ee9391f9cdc994f721b1b5cdc74
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2024-05-31 17:20:40 +0300

    EWPP-4370: Update tests in for existing content type with Contact entity.

--- tests/src/Kernel/PersonRenderTest.php
+++ tests/src/Kernel/PersonRenderTest.php
@@ -229,7 +229,7 @@ class PersonRenderTest extends ContentRenderTestBase {
           'body' => 'Fax number organisation_reference_contact',
         ], [
           'label' => 'Address',
-          'body' => 'Address organisation_reference_contact, 1001 Brussels, Belgium',
+          'body' => 'Address organisation_reference_contact 1001 Brussels Belgium',
         ], [
           'label' => 'Office',
           'body' => 'Office organisation_reference_contact',
@@ -326,7 +326,7 @@ class PersonRenderTest extends ContentRenderTestBase {
             'body' => 'Fax number direct_contact',
           ], [
             'label' => 'Address',
-            'body' => 'Address direct_contact, 1001 Brussels, Belgium',
+            'body' => 'Address direct_contact 1001 Brussels Belgium',
           ], [
             'label' => 'Office',
             'body' => 'Office direct_contact',
