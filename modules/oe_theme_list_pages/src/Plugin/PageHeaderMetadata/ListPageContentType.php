3d0121fadfe92c0cd82d6cac509a58183c67c502
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2024-09-10 14:59:53 +0300

    EWPP-4704: Remove override of method getMetadata() for List Page content type.

--- modules/oe_theme_list_pages/src/Plugin/PageHeaderMetadata/ListPageContentType.php
+++ modules/oe_theme_list_pages/src/Plugin/PageHeaderMetadata/ListPageContentType.php
@@ -26,34 +26,4 @@ class ListPageContentType extends NodeViewRoutesBase {
     return $node && $node->bundle() === 'oe_list_page';
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function getMetadata(): array {
-    $metadata = parent::getMetadata();
-
-    $node = $this->getNode();
-    if ($node->get('oe_summary')->isEmpty()) {
-      return $metadata;
-    }
-
-    $summary = $node->get('oe_summary')->first();
-    $metadata['introduction'] = [
-      // We strip the tags because the component expects only one paragraph of
-      // text and the field is using a text format which adds paragraph tags.
-      '#type' => 'inline_template',
-      '#template' => '{{ summary|render|striptags("<strong><a><em>")|raw }}',
-      '#context' => [
-        'summary' => [
-          '#type' => 'processed_text',
-          '#text' => $summary->value,
-          '#format' => $summary->format,
-          '#langcode' => $summary->getLangcode(),
-        ],
-      ],
-    ];
-
-    return $metadata;
-  }
-
 }
