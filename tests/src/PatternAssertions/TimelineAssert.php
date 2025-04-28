814ca0c9df107415d7089d435ff00be1729ddf37
22Alexandra <beldean.ale@gmail.com>
2025-04-28 12:13:25 +0300

    EWPP-0001: Fix color mode assertion for pattern assertion classes.

--- tests/src/PatternAssertions/TimelineAssert.php
+++ tests/src/PatternAssertions/TimelineAssert.php
@@ -98,13 +98,15 @@ class TimelineAssert extends BasePatternAssert {
       'purple-violet',
       'red-tomato',
     ];
-    if (!in_array($color_mode, $color_modes)) {
-      throw new Exception("The color mode '$color_mode' is not supported.");
-    }
     if (empty($color_mode)) {
       foreach ($color_modes as $mode) {
         $this->assertElementNotExists("ol.ecl-timeline.ecl-color-mode--$mode", $crawler);
       }
+      return;
+    }
+
+    if (!in_array($color_mode, $color_modes)) {
+      throw new Exception("The color mode '$color_mode' is not supported.");
     }
 
     $this->assertElementExists("ol.ecl-timeline.ecl-color-mode--$color_mode", $crawler);
