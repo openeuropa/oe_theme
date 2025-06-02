e1e1d2f90f76fdbd74643037ade23cdd1d81614c
Antonio De Marco <antonio@nuvole.org>
2025-06-02 14:02:15 +0200

    EWPP-4648: Revert EnsurePHPUnitBatchingTestExtension.

--- tests/src/EnsurePHPUnitBatchingTestExtension.php
+++ tests/src/EnsurePHPUnitBatchingTestExtension.php
@@ -5,37 +5,32 @@ declare(strict_types=1);
 namespace Drupal\Tests\oe_theme;
 
 use PHPUnit\Framework\TestCase;
-use PHPUnit\Event\Test\Prepared;
-use PHPUnit\Event\Test\PreparedSubscriber;
-use PHPUnit\Event\Code\TestMethod;
+use PHPUnit\Runner\BeforeTestHook;
+use PHPUnit\Runner\Exception;
 
 /**
  * Check if a test has been assigned to a test batch.
+ *
+ * @todo This class should be removed and approach changed to new event system during upgrade to phpunit 10.
+ * See https://localheinz.com/articles/2023/02/14/extending-phpunit-with-its-new-event-system/#content-components-in-the-new-event-system
  */
-final class EnsurePHPUnitBatchingTestExtension implements PreparedSubscriber {
+class EnsurePHPUnitBatchingTestExtension implements BeforeTestHook {
 
   /**
    * {@inheritdoc}
    */
-  public function notify(Prepared $event): void {
-    $test = $event->test();
-    if (!$test instanceof TestMethod) {
-      return;
-    }
-
-    $className = $test->className();
-    $reflection = new \ReflectionClass($className);
+  public function executeBeforeTest(string $test): void {
+    [$class] = \explode('::', $test);
+    $reflection = new \ReflectionClass($class);
     if (!$reflection->isSubclassOf(TestCase::class)) {
       return;
     }
-
     $doc_comment = $reflection->getDocComment();
     if ($doc_comment === FALSE) {
-      throw new \RuntimeException("The following test has no doc comment: {$className}");
+      throw new Exception("The following test has no doc comment: " . $test);
     }
-
-    if (!preg_match('/@group batch\d+/', $doc_comment)) {
-      throw new \RuntimeException("The following test has not been assigned to a test batch: {$className}");
+    if (!(bool) preg_match('/@group batch(\d+)/', $doc_comment)) {
+      throw new Exception("The following test has not been assigned to a test batch: " . $test);
     }
   }
 
