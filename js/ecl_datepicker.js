df85322f5b0b14dbe01b8855d86b05d842fb9eb3
Raphael Tenenbaum <raphael.tenenbaum@ext.ec.europa.eu>
2025-06-13 13:23:04 +0000

    OPENEUROPA-1605: Fix ecl_datepicker drupal behaviors attach conflict

--- js/ecl_datepicker.js
+++ js/ecl_datepicker.js
@@ -5,12 +5,14 @@
 (function (ECL, Drupal) {
   Drupal.behaviors.eclDatepicker = {
     attach: function attach(context, settings) {
-      var elements = document.querySelectorAll('[data-ecl-datepicker-toggle]');
+      var elements = context.querySelectorAll('[data-ecl-datepicker-toggle]');
       for (var i = 0; i < elements.length; i++) {
-        var datepicker = new ECL.Datepicker(elements[i], {
-          format: settings.oe_theme.ecl_datepicker_format
-        });
-        datepicker.init();
+        if(!elements[i].hasAttribute('data-ecl-auto-initialized')) {
+          var datepicker = new ECL.Datepicker(elements[i], {
+            format: settings.oe_theme.ecl_datepicker_format
+          });
+          datepicker.init();
+        }
       }
     }
   };
